423e3ee3da88004649b43cc2d69d7adb2753d835
Gael varoquaux <gael.varoquaux@normalesup.org>
2010-08-03 14:43:04 +0200

    ENH: Add a tiny ORM to store meta-data in a sqlite db.

---
+++ joblib/test/test_cache_db.py
@@ -0,0 +1,38 @@
+"""
+Test the cache_db module.
+"""
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org> 
+# Copyright (c) 2009 Gael Varoquaux
+# License: BSD Style, 3 clauses.
+
+import nose
+
+from ..cache_db import CacheDB
+
+
+################################################################################
+# Tests
+def test_store_retrieve():
+    """ A simple test for CacheDb
+    """
+    db = CacheDB()
+    e = dict(key='key', func_name='foo', module='bar', 
+              args='', creation_time=10, access_time=20, 
+              computation_time=5, size=100, last_cost=.5)
+    db.new_entry(e)
+    d = db.get('key')
+    yield nose.tools.assert_equal, d, e
+    db.update_entry('key', args='2')
+    yield nose.tools.assert_equal, '2', db.get('key')['args']
+    db.remove('key')
+    yield nose.tools.assert_raises, KeyError, db.get, 'key'
+    yield nose.tools.assert_raises, KeyError, db.remove, 'key'
+    e.pop('module')
+    yield nose.tools.assert_raises, KeyError, db.new_entry, e
+    # Smoke test the two remaining functions
+    db.sync()
+    db.clear()
+ 
+
+
