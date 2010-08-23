6ebaca930149b3a496017b620f608efc0d664944
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-08-23 14:53:40 +0200

    ENH: Fix disk_used and add a test

--- joblib/disk.py
+++ joblib/disk.py
@@ -14,11 +14,13 @@ def disk_used(path):
     """ Return the disk usage in a directory. 
     """
     size = 0
-    for file in os.listdir(path):
-        size += os.stat(os.path.join(path, file)).st_size
+    for file in os.listdir(path) + ['.']:
+        stat =  os.stat(os.path.join(path, file))
+        size += stat.st_blocks * 512
     # We need to convert to int to avoid having longs on some systems (we
     # don't want longs to avoid problems we SQLite)
-    return int(size/1024)
+    print path, size
+    return int(size/1024.)
 
 
 def disk_free(path):
@@ -51,7 +53,7 @@ def memstr_to_kbytes(text):
     units = dict(K=1, M=kilo, G=kilo**2)
     try:
         size = int(units[text[-1]]*float(text[:-1]))
-    except StopIteration:
+    except (KeyError, ValueError):
         raise ValueError(
                 "Invalid literal for size give: %s (type %s) should be "
                 "alike '10G', '500M', '50K'." % (text, type(text))
--- joblib/test/test_disk.py
+++ joblib/test/test_disk.py
@@ -6,9 +6,37 @@ Unit tests for the disk utilities.
 # Copyright (c) 2010 Gael Varoquaux
 # License: BSD Style, 3 clauses.
 
+import os
+import shutil
+import array
+from tempfile import mkdtemp
+
 import nose
 
-from ..disk import memstr_to_kbytes
+from ..disk import memstr_to_kbytes, disk_used
+
+################################################################################
+
+def test_disk_used():
+    cachedir = mkdtemp()
+    try:
+        if os.path.exists(cachedir):
+            shutil.rmtree(cachedir)
+        os.mkdir(cachedir)
+        # Not write a file that is 1M big in this directory, and check the
+        # size. The reason we use such a big file is that it makes us robust
+        # to errors due to block allocation.
+        a = array.array('i')
+        sizeof_i = a.itemsize
+        target_size = 1024
+        n = target_size*1024/sizeof_i
+        a = array.array('i', n*(1,))
+        a.tofile(file(os.path.join(cachedir, 'test'), 'w'))
+        nose.tools.assert_true(disk_used(cachedir) > target_size)
+        nose.tools.assert_true(disk_used(cachedir) < target_size + 12)
+    finally:
+        shutil.rmtree(cachedir)
+
 
 def test_memstr_to_kbytes():
     for text, value in zip(('80G', '1.4M', '120M', '53K'),
