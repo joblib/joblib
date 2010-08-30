d151fc84a54e443b5d2885b864bef949d538702a
Pietro Berkes <pietro.berkes@googlemail.com>
2010-08-31 05:13:46 +0800

    MISC: JoblibException moved to accessible place

---
+++ joblib/common.py
@@ -0,0 +1,27 @@
+"""
+Common tools and utils.
+"""
+# Author: Gael Varoquaux < gael dot varoquaux at normalesup dot org >
+# Copyright: 2010, Gael Varoquaux
+# License: BSD 3 clause
+
+class JoblibException(Exception):
+    """ A simple exception with an error message that you can get to.
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __reduce__(self):
+        # For pickling
+        return self.__class__, (self.message,), {}
+
+    def __repr__(self):
+        return '%s\n%s\n%s\n%s' % (
+                    self.__class__.__name__,
+                    75*'_',
+                    self.message,
+                    75*'_')
+
+    __str__ = __repr__
+
--- joblib/parallel.py
+++ joblib/parallel.py
@@ -20,29 +20,9 @@ except ImportError:
 
 from .format_stack import format_exc, format_outer_frames
 from .logger import Logger, short_format_time
+from .common import JoblibException
 
 ################################################################################
-
-class JoblibException(Exception):
-    """ A simple exception with an error message that you can get to.
-    """
-
-    def __init__(self, message):
-        self.message = message
-
-    def __reduce__(self):
-        # For pickling
-        return self.__class__, (self.message,), {}
-
-    def __repr__(self):
-        return '%s\n%s\n%s\n%s' % (
-                    self.__class__.__name__,
-                    75*'_',
-                    self.message,
-                    75*'_')
-
-    __str__ = __repr__
-
 
 class SafeFunction(object):
     """ Wraps a function to make it exception with full traceback in
