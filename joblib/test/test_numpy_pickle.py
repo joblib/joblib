4b718d147be2c1f7ac78ea66ed0f2757c4ca934c
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-12-21 10:57:09 +0100

    ENH: add zip-based pickling

--- joblib/test/test_numpy_pickle.py
+++ joblib/test/test_numpy_pickle.py
@@ -113,36 +113,41 @@ def test_standard_types():
     #""" Test pickling and saving with standard types.
     #"""
     filename = env['filename']
-    for member in typelist:
-        numpy_pickle.dump(member, filename)
-        _member = numpy_pickle.load(filename)
-        # We compare the pickled instance to the reloaded one only if it
-        # can be compared to a copied one
-        if member == copy.deepcopy(member):
-            yield nose.tools.assert_equal, member, _member
+    for dump, load in [(numpy_pickle.dump, numpy_pickle.load),
+                       (numpy_pickle.dumpz, numpy_pickle.loadz)]:
+        for member in typelist:
+            dump(member, filename)
+            _member = load(filename)
+            # We compare the pickled instance to the reloaded one only if it
+            # can be compared to a copied one
+            if member == copy.deepcopy(member):
+                yield nose.tools.assert_equal, member, _member
 
 
 @with_numpy
 def test_numpy_persistence():
     filename = env['filename']
     a = np.random.random(10)
-    for obj in (a,), (a, a), [a, a, a]:
-        filenames = numpy_pickle.dump(obj, filename)
-        # Check that one file was created per array
-        yield nose.tools.assert_equal, len(filenames), len(obj) + 1
-        # Check that these files do exist
-        for file in filenames:
-            yield nose.tools.assert_true, \
-                os.path.exists(os.path.join(env['dir'], file))
+    for dump, load in [(numpy_pickle.dump, numpy_pickle.load),
+                       (numpy_pickle.dumpz, numpy_pickle.loadz)]:
+        for obj in (a,), (a, a), [a, a, a]:
+            filenames = dump(obj, filename)
+            if dump is numpy_pickle.dump:
+                # Check that one file was created per array
+                yield nose.tools.assert_equal, len(filenames), len(obj) + 1
+                # Check that these files do exist
+                for file in filenames:
+                    yield nose.tools.assert_true, \
+                        os.path.exists(os.path.join(env['dir'], file))
 
-        # Unpickle the object
-        obj_ = numpy_pickle.load(filename)
-        # Check that the items are indeed arrays
-        for item in obj_:
-            yield nose.tools.assert_true, isinstance(item, np.ndarray)
-        # And finally, check that all the values are equal.
-        yield nose.tools.assert_true, np.all(np.array(obj) ==
-                                             np.array(obj_))
+            # Unpickle the object
+            obj_ = load(filename)
+            # Check that the items are indeed arrays
+            for item in obj_:
+                yield nose.tools.assert_true, isinstance(item, np.ndarray)
+            # And finally, check that all the values are equal.
+            yield nose.tools.assert_true, np.all(np.array(obj) ==
+                                                np.array(obj_))
 
 
 @with_numpy
@@ -165,3 +170,5 @@ def test_masked_array_persistence():
     numpy_pickle.dump(a, filename)
     b = numpy_pickle.load(filename, mmap_mode='r')
     nose.tools.assert_true, isinstance(b, np.ma.masked_array)
+
+
