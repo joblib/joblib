8966715e86b9c6613d90366ca24129ee5522f963
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-24 17:29:13 +0200

    Skip parallel_numpy.rst doctests when multiprocessing is disabled

--- doc/parallel_numpy_fixture.py
+++ doc/parallel_numpy_fixture.py
@@ -1,13 +1,17 @@
 """Fixture module to skip memmaping test if numpy is not installed"""
 
+import os
 from nose import SkipTest
 from joblib.test.common import setup_autokill
 from joblib.test.common import teardown_autokill
 
 
 def setup_module(module):
+    if not int(os.environ.get('JOBLIB_MULTIPROCESSING', 1)):
+        raise SkipTest(
+            'Skipped as multiprocessing is required to run this doctest.')
     try:
-        import numpy as np
+        import numpy
         import multiprocessing
     except ImportError:
         raise SkipTest('Skipped as numpy or multiprocessing is not installed')
