595b5c9e0487506225b907f97d31bc871762acf4
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-25 01:50:29 +0200

    Skip numpy memmap pool tests if multiprocessing does not support semaphores

--- doc/parallel_numpy_fixture.py
+++ doc/parallel_numpy_fixture.py
@@ -10,10 +10,26 @@ def setup_module(module):
     if not int(os.environ.get('JOBLIB_MULTIPROCESSING', 1)):
         raise SkipTest(
             'Skipped as multiprocessing is required to run this doctest.')
-    try:
-        import numpy
-        import multiprocessing
-    except ImportError:
+
+    numpy = None
+    multiprocessing = int(os.environ.get('JOBLIB_MULTIPROCESSING', 1)) or None
+    if multiprocessing:
+        try:
+            import multiprocessing
+        except ImportError:
+            multiprocessing = None
+
+    # validate that locking is available on the system and
+    # skip test if not
+    if multiprocessing:
+        try:
+            import numpy
+            _sem = multiprocessing.Semaphore()
+            del _sem  # cleanup
+        except (ImportError, OSError):
+            multiprocessing = None
+
+    if numpy is None or multiprocessing is None:
         raise SkipTest('Skipped as numpy or multiprocessing is not installed')
 
     setup_autokill(module.__name__)
