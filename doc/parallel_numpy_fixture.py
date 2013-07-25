331ffb1b8dcb39f863fd348a3e9ad49ba764db5f
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-25 19:01:45 +0200

    Cleanup duplicated code

--- doc/parallel_numpy_fixture.py
+++ doc/parallel_numpy_fixture.py
@@ -1,36 +1,20 @@
 """Fixture module to skip memmaping test if numpy is not installed"""
 
-import os
 from nose import SkipTest
+from joblib.parallel import multiprocessing
 from joblib.test.common import setup_autokill
 from joblib.test.common import teardown_autokill
 
 
 def setup_module(module):
-    if not int(os.environ.get('JOBLIB_MULTIPROCESSING', 1)):
-        raise SkipTest(
-            'Skipped as multiprocessing is required to run this doctest.')
-
     numpy = None
-    multiprocessing = int(os.environ.get('JOBLIB_MULTIPROCESSING', 1)) or None
-    if multiprocessing:
-        try:
-            import multiprocessing
-        except ImportError:
-            multiprocessing = None
-
-    # validate that locking is available on the system and
-    # skip test if not
-    if multiprocessing:
-        try:
-            import numpy
-            _sem = multiprocessing.Semaphore()
-            del _sem  # cleanup
-        except (ImportError, OSError):
-            multiprocessing = None
+    try:
+        import numpy
+    except ImportError:
+        pass
 
     if numpy is None or multiprocessing is None:
-        raise SkipTest('Skipped as numpy or multiprocessing is not installed')
+        raise SkipTest('Skipped as numpy or multiprocessing is not available')
 
     setup_autokill(module.__name__)
 
