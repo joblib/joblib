56c049d5868bdb6fcfbcf016833c788dd91ba72b
Olivier Grisel <olivier.grisel@ensta.org>
2013-12-10 16:13:23 +0100

    FIX #97: Pickler.save_global API has changed in Python 3.4

--- joblib/hashing.py
+++ joblib/hashing.py
@@ -83,10 +83,13 @@ class Hasher(Pickler):
         # We have to override this method in order to deal with objects
         # defined interactively in IPython that are not injected in
         # __main__
+        kwargs = dict(name=name, pack=pack)
+        if sys.version_info >= (3, 4):
+            del kwargs['pack']
         try:
-            Pickler.save_global(self, obj, name=name, pack=pack)
+            Pickler.save_global(self, obj, **kwargs)
         except pickle.PicklingError:
-            Pickler.save_global(self, obj, name=name, pack=pack)
+            Pickler.save_global(self, obj, **kwargs)
             module = getattr(obj, "__module__", None)
             if module == '__main__':
                 my_name = name
