cb65777b9ee993fc5f2e23aee027349292b29d56
Loïc Estève <loic.esteve@ymail.com>
2015-03-26 11:09:25 +0100

    Switch to 0.9 dev version
    Changing minor version since we are dropping python 3.2 support

--- joblib/__init__.py
+++ joblib/__init__.py
@@ -100,7 +100,23 @@ Main features
 
 """
 
-__version__ = '0.8.4'
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.9.0.dev0'
 
 
 from .memory import Memory, MemorizedResult
