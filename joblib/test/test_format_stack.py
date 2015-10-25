54b5794dabe3bbf7a5ad1eebde32b0fb6234fa4c
Martin Frodl <mfrodl@redhat.com>
2015-10-25 17:33:55 +0100

    Prevent tokenizing .pyc files in format_records

--- joblib/test/test_format_stack.py
+++ joblib/test/test_format_stack.py
@@ -7,8 +7,9 @@ Unit tests for the stack formatting utilities
 # License: BSD Style, 3 clauses.
 
 import nose
+import sys
 
-from joblib.format_stack import safe_repr
+from joblib.format_stack import safe_repr, _fixed_getframes, format_records
 
 
 ###############################################################################
@@ -20,3 +21,23 @@ class Vicious(object):
 
 def test_safe_repr():
     safe_repr(Vicious())
+
+def test_format_records():
+    try:
+        raise ValueError
+    except ValueError:
+        etb = sys.exc_info()[2]
+        records = _fixed_getframes(etb)
+
+        # Modify filenames in traceback records from .py to .pyc
+        pyc_records = []
+        for record in records:
+            if record[1].endswith('.py'):
+                record = list(record)
+                record[1] += 'c'
+                record = tuple(record)
+                pyc_records.append(record)
+
+        # Check that no .pyc files are listed in traceback
+        for fmt_rec in format_records(pyc_records):
+            assert '.pyc in' not in fmt_rec
