634b1ec2b123c23b7ca29eee6eacf2c2775534bc
Loïc Estève <loic.esteve@ymail.com>
2015-11-03 09:32:29 +0100

    TST Improve test_format_records test
    Add a function with a non-trivial stack and check each level of the
    stack more thoroughly.
    
    Plus some minor clean-up.

--- joblib/test/test_format_stack.py
+++ joblib/test/test_format_stack.py
@@ -22,22 +22,44 @@ class Vicious(object):
 def test_safe_repr():
     safe_repr(Vicious())
 
+
+def _change_file_extensions_to_pyc(record):
+    _1, filename, _2, _3, _4, _5 = record
+    if filename.endswith('.py'):
+        filename += 'c'
+    return _1, filename, _2, _3, _4, _5
+
+
+def _raise_exception(a, b):
+    """Function that raises with a non trivial call stack
+    """
+    def helper(a, b):
+        raise ValueError('Nope, this can not work')
+
+    helper(a, b)
+
+
 def test_format_records():
     try:
-        raise ValueError
+        _raise_exception('a', 42)
     except ValueError:
         etb = sys.exc_info()[2]
         records = _fixed_getframes(etb)
 
         # Modify filenames in traceback records from .py to .pyc
-        pyc_records = []
-        for record in records:
-            if record[1].endswith('.py'):
-                record = list(record)
-                record[1] += 'c'
-                record = tuple(record)
-                pyc_records.append(record)
+        pyc_records = [_change_file_extensions_to_pyc(record)
+                       for record in records]
 
-        # Check that no .pyc files are listed in traceback
-        for fmt_rec in format_records(pyc_records):
-            assert '.pyc in' not in fmt_rec
+        formatted_records = format_records(pyc_records)
+
+        # Check that the .py file and not the .pyc one is listed in
+        # the traceback
+        for fmt_rec in formatted_records:
+            assert 'test_format_stack.py in' in fmt_rec
+
+        # Check exception stack
+        assert "_raise_exception('a', 42)" in formatted_records[0]
+        assert 'helper(a, b)' in formatted_records[1]
+        assert "a = 'a'" in formatted_records[1]
+        assert 'b = 42' in formatted_records[1]
+        assert 'Nope, this can not work' in formatted_records[2]
