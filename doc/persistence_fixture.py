99c597c0b19d0b4cb70036862658a0e0529ed8e0
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-06-29 16:23:44 +0200

    commenting python 2.6 skip in persitence documentation

--- doc/persistence_fixture.py
+++ doc/persistence_fixture.py
@@ -7,4 +7,7 @@ from joblib import _compat
 def setup_module(module):
     """Setup module."""
     if _compat.PY26:
+        # gzip.GZipFile and bz2.BZ2File compressor classes cannot be used
+        # within a context manager (e.g in a `with` block) in python 2.6 so
+        # we skip doctesting of persistence documentation in this case.
         raise SkipTest("Skipping persistence doctest in Python 2.6")
