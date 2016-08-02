ddb28b4baf61c72fc7f00c12a75cde2f54f72f74
Vincent Latrouite <vincent.latrouite@gmail.com>
2016-08-02 12:06:08 +0200

    [numpy_pickle_utils] Checking unicode type only if we have a version inferior to python3.

--- joblib/numpy_pickle_utils.py
+++ joblib/numpy_pickle_utils.py
@@ -299,7 +299,7 @@ class BinaryZlibFile(io.BufferedIOBase):
         else:
             raise ValueError("Invalid mode: %r" % (mode,))
 
-        if isinstance(filename, (str, bytes, unicode)):
+        if isinstance(filename, (str, bytes, (sys.version_info < (3,) and unicode) or str)):
             self._fp = open(filename, mode)
             self._closefp = True
             self._mode = mode_code
