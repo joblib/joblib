1e4232b70db56964352c7eb452882f4b3063be67
Karan Desai <karandesai_96@live.com>
2016-12-12 20:29:53 +0530

    Replace setup-teardown fixtures with tmpdir in test_logger.py (#453)

--- joblib/test/test_logger.py
+++ joblib/test/test_logger.py
@@ -6,11 +6,8 @@ Test the logger module.
 # Copyright (c) 2009 Gael Varoquaux
 # License: BSD Style, 3 clauses.
 
-import shutil
-import os
 import sys
 import io
-from tempfile import mkdtemp
 import re
 
 from joblib.logger import PrintTime
@@ -21,41 +18,20 @@ try:
 except NameError:
     unicode = lambda s: s
 
-###############################################################################
-# Test fixtures
-env = dict()
 
-
-def setup():
-    """ Test setup.
-    """
-    cachedir = mkdtemp()
-    if os.path.exists(cachedir):
-        shutil.rmtree(cachedir)
-    env['dir'] = cachedir
-
-
-def teardown():
-    """ Test teardown.
-    """
-    #return True
-    shutil.rmtree(env['dir'])
-
-
-###############################################################################
-# Tests
-def test_print_time():
+def test_print_time(tmpdir):
     # A simple smoke test for PrintTime.
+    logfile = tmpdir.join('test.log').strpath
     try:
         orig_stderr = sys.stderr
         sys.stderr = io.StringIO()
-        print_time = PrintTime(logfile=os.path.join(env['dir'], 'test.log'))
+        print_time = PrintTime(logfile=logfile)
         print_time(unicode('Foo'))
         # Create a second time, to smoke test log rotation.
-        print_time = PrintTime(logfile=os.path.join(env['dir'], 'test.log'))
+        print_time = PrintTime(logfile=logfile)
         print_time(unicode('Foo'))
         # And a third time
-        print_time = PrintTime(logfile=os.path.join(env['dir'], 'test.log'))
+        print_time = PrintTime(logfile=logfile)
         print_time(unicode('Foo'))
         printed_text = sys.stderr.getvalue()
         # Use regexps to be robust to time variations
