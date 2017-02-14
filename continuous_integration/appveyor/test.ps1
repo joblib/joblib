ba4de49b0ceb9b10ea4c85f6b07816bcc012eb1f
Loïc Estève <loic.esteve@ymail.com>
2017-02-14 18:14:54 +0100

    Enable codecov on AppVeyor

---
+++ continuous_integration/appveyor/test.ps1
@@ -0,0 +1,6 @@
+$installed_joblib_folder = $(python -c "import os; os.chdir('c:/'); import joblib;\
+    print(os.path.dirname(joblib.__file__))")
+echo "joblib found in: $installed_joblib_folder"
+# --pyargs argument is used to make sure we run the tests on the
+# installed package rather than on the local folder
+py.test --pyargs joblib --cov $installed_joblib_folder
