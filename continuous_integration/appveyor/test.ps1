22155517a64b288a94057219d9b509dd9c7a59fa
Loïc Estève <loic.esteve@ymail.com>
2017-03-02 00:47:36 +0100

    AppVeyor test.ps1 should exit with pytest's exit code

--- continuous_integration/appveyor/test.ps1
+++ continuous_integration/appveyor/test.ps1
@@ -4,3 +4,4 @@ echo "joblib found in: $installed_joblib_folder"
 # --pyargs argument is used to make sure we run the tests on the
 # installed package rather than on the local folder
 pytest --pyargs joblib --cov $installed_joblib_folder
+exit $LastExitCode
