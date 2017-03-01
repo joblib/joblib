16d321895459a557e70e6cf5c3895a7bb4261d21
Thomas Moreau <toma.moral@gmail.com>
2017-03-01 11:48:42 -0500

    FIX remove unwanted warnings

--- joblib/test/test_parallel.py
+++ joblib/test/test_parallel.py
@@ -151,6 +151,11 @@ def test_main_thread_renamed_no_warning(backend, monkeypatch):
         results = Parallel(n_jobs=2, backend=backend)(
             delayed(square)(x) for x in range(3))
         assert results == [0, 1, 4]
+
+    # Due to the default parameters of LokyBackend, there is a change that
+    # warninfo catches Warnings from worker timeouts. We remove it if it exists
+    warninfo = [w for w in warninfo if "worker timeout" not in w.message]
+
     # The multiprocessing backend will raise a warning when detecting that is
     # started from the non-main thread. Let's check that there is no false
     # positive because of the name change.
