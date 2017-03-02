0778447ef7d87a07d4166f3cd942ece9826bf142
Thomas Moreau <toma.moral@gmail.com>
2017-03-02 09:26:50 -0500

    CI add timeout to appveyor pytest

--- continuous_integration/appveyor/test.ps1
+++ continuous_integration/appveyor/test.ps1
@@ -3,4 +3,4 @@ $installed_joblib_folder = $(python -c "import os; os.chdir('c:/'); import jobli
 echo "joblib found in: $installed_joblib_folder"
 # --pyargs argument is used to make sure we run the tests on the
 # installed package rather than on the local folder
-pytest --pyargs joblib --cov $installed_joblib_folder
+pytest --pyargs joblib --cov $installed_joblib_folder -l --timeout 15
