4b968b5e6a8f33779efe5ea84417070ab4d8c991
Loïc Estève <loic.esteve@ymail.com>
2018-01-18 09:36:20 +0100

    [MRG] Only run doctests for numpy >= 1.14 (#584)

--- continuous_integration/travis/test_script.sh
+++ continuous_integration/travis/test_script.sh
@@ -7,14 +7,6 @@ if [[ -n "$FLAKE8_VERSION" ]]; then
 fi
 
 if [[ "$SKIP_TESTS" != "true" ]]; then
-    if [ -z "$NUMPY_VERSION" ]; then
-        # We want to disable doctests because they need numpy to
-        # run. I could not find a way to override the
-        # --doctest-modules in setup.cfg so we remove the
-        # doctest-related lines in setup.cfg instead
-        sed -i '/--doctest/d' setup.cfg
-    fi
-
     if [ "$COVERAGE" == "true" ]; then
         # Add coverage option to setup.cfg file if current test run
         # has to generate report for codecov ...
