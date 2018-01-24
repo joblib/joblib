55a1bd732759ce37c91defd98333575cb3ca8b3f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-01-24 22:26:23 +0100

    DOC fix spelling

--- examples/parallel_profiling.py
+++ examples/parallel_profiling.py
@@ -4,13 +4,13 @@ Memory and CPU consumption using joblib.Parallel
 ================================================
 
 This example illustrates some important aspects to keep in mind when using
-:class:`joblib.Parallel`. We will focus on different patterns resulting to the
+:class:`joblib.Parallel`. We will focus on different patterns resulting in the
 same outcomes but which trigger different internal behaviors. Those differences
 will impact the memory consumption and computation time.
 
 To illustrate those differences, the moving average from a large array will be
 computed. Therefore, the average can be computed in parallel at different
-location of the data array.
+locations of the data array.
 
 """
 
@@ -18,7 +18,7 @@ location of the data array.
 # ``time_memory_profiling`` and ``print_info`` are two helper functions being
 # used extensively later on. The former function allows to track the memory
 # consumption and computation time during the call of a function while the
-# latter output profiling information. To have a more accurate estimate of
+# latter outputs profiling information. To have a more accurate estimate of
 # those statistics, the given function is executed several times.
 
 import gc
@@ -56,78 +56,80 @@ def time_memory_profiling(func, *args, **kwargs):
     gc.collect()
     n_iter = 3
     tic = time.time()
-    mem_use = [memory_usage((func, args, kwargs), interval=.00001,
-                            multiprocess=True, include_children=True)
-               for _ in range(n_iter)]
+    mem_use = []
+    for _ in range(n_iter):
+        gc.collect()
+        mem_use.append(
+            memory_usage((func, args, kwargs), interval=.001,
+                         multiprocess=True, include_children=True))
     toc = time.time()
     return (np.mean([max(mem_use_it) - min(mem_use_it)
                      for mem_use_it in mem_use]),
             (toc - tic) / n_iter)
 
 
-def print_info(mem_used_avg, elapsed_time):
-    print('Memory used by slicing the data in the parent'
-          ' function: {:.2f} MiB'.format(mem_used_avg))
+def print_info(mem_used_avg, elapsed_time, func_name):
+    print('Memory used by {}: {:.2f} MiB'.format(func_name, mem_used_avg))
     print('Elasped processing time: {:.2f} s\n'.format(elapsed_time))
 
 
 ###############################################################################
 # As explained in the introduction, we will create a large array
-# (i.e. ``data``) and take is average at different locations (i.e. ``slices``)
+# (i.e. ``data``) and take its average at different locations (i.e. ``slices``)
 # using a specific window size (i.e. ``window_size``). To speed-up the
 # execution, we under-sample the number of slices to evaluate.
 
 import numpy as np
 
-data = np.random.random((10000000,))
-window_size = 500000
+data = np.random.random((int(1e7),))
+window_size = int(5e5)
 slices = [slice(start, start + window_size)
-          for start in range(0, data.size - window_size, 50000)]
+          for start in range(0, data.size - window_size, int(5e4))]
 
 
 ###############################################################################
 # In the first strategy, the function passed to :class:`joblib.Parallel` will
-# take the portion of the data from which we need to compute the
-# sum. Therefore, the function passed to parallel only compute the sum of the
+# take the portion of the data for which we need to compute the
+# sum. Therefore, the function passed to parallel only computes the sum of the
 # array passed as argument. Therefore, all arrays passed to the
-# ``moving_average`` function will always be different and it will not be able
+# ``average`` function will always be different and it will not be able
 # to share memory between the workers.
 
 from joblib import Parallel, delayed
 
 
-def process_slicing_parent(data, window_size, slices):
-    def moving_average(data):
-        return data.sum()
+def average_slicing_parent(data, window_size, slices):
+    def average(data):
+        return data.mean()
 
-    Parallel(n_jobs=2, max_nbytes=0)(delayed(
-        moving_average)(data[sl]) for sl in slices)
+    Parallel(n_jobs=2, max_nbytes=0)(delayed(average)(data[sl])
+                                     for sl in slices)
 
 
-mem_used, elapsed_time = time_memory_profiling(process_slicing_parent, data,
+mem_used, elapsed_time = time_memory_profiling(average_slicing_parent, data,
                                                window_size, slices)
-print_info(mem_used, elapsed_time)
+print_info(mem_used, elapsed_time, average_slicing_parent.__name__)
 
 
 ###############################################################################
 # The second strategy differs from the former since the original large array
-# will be passed to the ``moving_average`` function and the slicing will occur
+# will be passed to the ``average`` function and the slicing will occur
 # directly in child processes. Therefore, the original array can be shared
 # between the different workers and will lead to a more efficient use of the
 # memory.
 
 
-def process_slicing_children(data, window_size, slices):
-    def moving_average(data, sl):
-        return data[sl].sum()
+def average_slicing_children(data, window_size, slices):
+    def average(data, sl):
+        return data[sl].mean()
 
-    Parallel(n_jobs=2, max_nbytes=0)(delayed(
-        moving_average)(data, sl) for sl in slices)
+    Parallel(n_jobs=2, max_nbytes=0)(delayed(average)(data, sl)
+                                     for sl in slices)
 
 
-mem_used, elapsed_time = time_memory_profiling(process_slicing_children, data,
+mem_used, elapsed_time = time_memory_profiling(average_slicing_children, data,
                                                window_size, slices)
-print_info(mem_used, elapsed_time)
+print_info(mem_used, elapsed_time, average_slicing_children.__name__)
 
 ###############################################################################
 # As a conclusion, the amount of memory used is reduced by passing the large
@@ -135,9 +137,9 @@ print_info(mem_used, elapsed_time)
 # less intuitive. However, when looking at the execution time, this strategy is
 # more time consuming. This due to the fact that internally, a hash of the
 # shareable array will be computed to ensure that the data can be
-# shared. However, computing an hash on large amount of data is costly. The
+# shared. However, computing a hash on a large amount of data is costly. The
 # solution to avoid such checking is to manually memmap the input data and give
-# directly it to :class:`joblib.Parallel`.
+# it directly to :class:`joblib.Parallel`.
 
 import os
 import joblib
@@ -146,15 +148,15 @@ filename_memmap = 'data.pkl'
 joblib.dump(data, filename_memmap)
 data = joblib.load(filename_memmap, mmap_mode='r')
 
-mem_used, elapsed_time = time_memory_profiling(process_slicing_parent, data,
+mem_used, elapsed_time = time_memory_profiling(average_slicing_parent, data,
                                                window_size, slices)
-print_info(mem_used, elapsed_time)
+print_info(mem_used, elapsed_time, average_slicing_parent.__name__)
 
 ###############################################################################
 
-mem_used, elapsed_time = time_memory_profiling(process_slicing_children, data,
+mem_used, elapsed_time = time_memory_profiling(average_slicing_children, data,
                                                window_size, slices)
-print_info(mem_used, elapsed_time)
+print_info(mem_used, elapsed_time, average_slicing_children.__name__)
 
 os.remove(filename_memmap)
 
