474a2d21e562662581cae03fe38c0028923891f6
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-01-25 15:24:59 +0100

    DOC refactor example

--- examples/parallel_memmap.py
+++ examples/parallel_memmap.py
@@ -4,16 +4,14 @@ Usage of numpy.memmap with joblib.Parallel
 ==========================================
 
 This example shows how to preallocate data in memmap arrays both for input and
-output of the parallel worker processes.
+output of the parallel worker processes. When using a memmap as output, it is
+then possible to write in the same buffer.
 
 """
-import tempfile
-import shutil
-import os
-import numpy as np
 
-from joblib import Parallel, delayed
-from joblib import load, dump
+###############################################################################
+# ``sum_row`` will compute the sum for a row of ``input`` and will write the
+# results in ``output``.
 
 
 def sum_row(input, output, i):
@@ -23,43 +21,63 @@ def sum_row(input, output, i):
     output[i] = sum_
 
 
+###############################################################################
+# We create a large 2D array containing some data.
+
+import numpy as np
+
 rng = np.random.RandomState(42)
-folder = tempfile.mkdtemp()
+samples = rng.normal(size=(10, int(1e6)))
+
+###############################################################################
+# Prepare the folder where the memmap will be dumped.
+
+import os
+
+folder = './joblib_memmap'
 samples_name = os.path.join(folder, 'samples')
 sums_name = os.path.join(folder, 'sums')
+
+###############################################################################
+# Pre-allocate a writeable shared memory map as a container for the results of
+# the parallel computation and dump the samples buffer.
+
+from joblib import dump
+
+sums = np.memmap(sums_name, dtype=samples.dtype,
+                 shape=samples.shape[0], mode='w+')
+dump(samples, samples_name)
+
+###############################################################################
+# Release the reference on the original in memory array and replace it by a
+# reference to the memmap array so that the garbage collector can release the
+# memory before forking. gc.collect() is internally called in Parallel just
+# before forking.
+
+from joblib import load
+
+samples = load(samples_name, mmap_mode='r')
+
+###############################################################################
+# Fork the worker processes to perform computation concurrently
+
+from joblib import Parallel, delayed
+
+Parallel(n_jobs=4)(delayed(sum_row)(samples, sums, i)
+                   for i in range(samples.shape[0]))
+
+###############################################################################
+# Compare the results from the output buffer with the ground truth
+
+print("Expected sums computed in the parent process:\n {}"
+      .format(samples.sum(axis=1)))
+print("Actual sums computed by the worker processes:\n {}"
+      .format(sums))
+
+###############################################################################
+
+import shutil
 try:
-    # Generate some data and an allocate an output buffer
-    samples = rng.normal(size=(10, int(1e6)))
-
-    # Pre-allocate a writeable shared memory map as a container for the
-    # results of the parallel computation
-    sums = np.memmap(sums_name, dtype=samples.dtype,
-                     shape=samples.shape[0], mode='w+')
-
-    # Dump the input data to disk to free the memory
-    dump(samples, samples_name)
-
-    # Release the reference on the original in memory array and replace it
-    # by a reference to the memmap array so that the garbage collector can
-    # release the memory before forking. gc.collect() is internally called
-    # in Parallel just before forking.
-    samples = load(samples_name, mmap_mode='r')
-
-    # Fork the worker processes to perform computation concurrently
-    Parallel(n_jobs=4)(delayed(sum_row)(samples, sums, i)
-                       for i in range(samples.shape[0]))
-
-    # Compare the results from the output buffer with the ground truth
-    print("Expected sums computed in the parent process:")
-    expected_result = samples.sum(axis=1)
-    print(expected_result)
-
-    print("Actual sums computed by the worker processes:")
-    print(sums)
-
-    assert np.allclose(expected_result, sums)
-finally:
-    try:
-        shutil.rmtree(folder)
-    except:  # noqa
-        print("Failed to delete: " + folder)
+    shutil.rmtree(folder)
+except:  # noqa
+    print("Failed to delete: " + folder)
