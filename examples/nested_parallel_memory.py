11bd0f50d138f8e0ada5086b190f256b76f44178
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-01-26 22:08:43 +0100

    iter

--- examples/nested_parallel_memory.py
+++ examples/nested_parallel_memory.py
@@ -18,7 +18,7 @@ We illustrate how to cache intermediate computing results using
 import time
 
 
-def costly_column(data, column):
+def costly_compute(data, column):
     """Emulate a costly function by sleeping and returning a column."""
     time.sleep(2)
     return data[column]
@@ -26,14 +26,17 @@ def costly_column(data, column):
 
 def data_processing_mean(data, column):
     """Compute the mean of a column."""
-    return costly_column(data, column).mean()
+    return costly_compute(data, column).mean()
 
 
 ###############################################################################
-# Create some data
+# Create some data. We fixed the random seed to generate deterministic data
+# across Python session. Note that this is not necessary for this specific
+# example since the memory cache is cleared at the end of the session.
 
 import numpy as np
-data = np.random.randn(int(1e4), 4)
+rng = np.random.RandomState(42)
+data = rng.randn(int(1e4), 4)
 
 ###############################################################################
 # It is first possible to make the processing without caching or parallel
@@ -48,15 +51,15 @@ print('Elapsed time for the entire processing: {:.2f} s'
       .format(stop - start))
 
 ###############################################################################
-# ``costly_column`` is expensive to compute and it is used as an intermediate
+# ``costly_compute`` is expensive to compute and it is used as an intermediate
 # step in ``data_processing_mean``. Therefore, it is interesting to store the
-# intermediate results from ``costly_column`` using :class:`joblib.Memory`.
+# intermediate results from ``costly_compute`` using :class:`joblib.Memory`.
 
 from joblib import Memory
 
-cachedir = './cachedir'
-memory = Memory(cachedir=cachedir, verbose=0)
-costly_column = memory.cache(costly_column)
+location = './cachedir'
+memory = Memory(location=location, verbose=0)
+costly_compute_cached = memory.cache(costly_compute)
 
 ###############################################################################
 # Then, we execute the same processing in parallel and caching the intermediate
@@ -76,8 +79,8 @@ print('Elapsed time for the entire processing: {:.2f} s'
 ###############################################################################
 # By using 2 workers, we get a x2 speed-up compare to the sequential case. By
 # executing again the same process, the intermediate results obtained by
-# calling ``costly_column`` will be loaded from the cache instead of executing
-# the function.
+# calling ``costly_compute_cached`` will be loaded from the cache instead of
+# executing the function.
 
 start = time.time()
 results = Parallel(n_jobs=2)(delayed(
@@ -92,15 +95,15 @@ print('Elapsed time for the entire processing: {:.2f} s'
 # Reuse intermediate checkpoints
 ###############################################################################
 # 
-# Having cached the intermediate results of the ``costly_column`` function, we
-# are able to easily reuse them by calling the function. We define a new
-# processing which will take the maximum of the array returned by
-# ``costly_column`` instead of previously the mean.
+# Having cached the intermediate results of the ``costly_compute_cached``
+# function, we are able to easily reuse them by calling the function. We define
+# a new processing which will take the maximum of the array returned by
+# ``costly_compute_cached`` instead of previously the mean.
 
 
 def data_processing_max(data, column):
     """Compute the max of a column."""
-    return costly_column(data, column).max()
+    return costly_compute_cached(data, column).max()
 
 
 start = time.time()
@@ -114,8 +117,8 @@ print('Elapsed time for the entire processing: {:.2f} s'
 
 ###############################################################################
 # The processing time only corresponds to the execution of the ``max``
-# function. The internal call to ``costly_column`` is reloading the results
-# from the cache.
+# function. The internal call to ``costly_compute_cached`` is reloading the
+# results from the cache.
 
 ###############################################################################
 # Clean-up the cache folder
