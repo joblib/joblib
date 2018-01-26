6c5a424b8607ef176cde82734cc0e5ef6a59def5
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-01-26 14:39:30 +0100

    sleeping examples

--- examples/memory_basic_usage.py
+++ examples/memory_basic_usage.py
@@ -12,79 +12,56 @@ usages with a function and a method.
 # Without :class:`joblib.Memory`
 ###############################################################################
 # 
-# To show the benefit of using :class:`joblib.Memory`, we will reduce the
-# dimension of some data using a principal components analysis (i.e. ``pca``).
-# Indeed, this example will greatly benefit from caching since the internal
-# computation in ``pca`` is expensive. First, we will check the time required
-# to perform the decomposition on a large array of data.
+# To show the benefit of using :class:`joblib.Memory`, we will implement a
+# function which is computationally expensive to execute. First, we will check
+# the time required to perform the decomposition on a large array of data.
 
 import time
 import numpy as np
 
 
-def pca(data, n_components=2):
-    """Principal components analysis.
-
-    Parameters
-    ----------
-    data : ndarray
-        The data to decompose.
-
-    n_components : int, default=2
-        The number of components to keep.
-
-    Return
-    ------
-    data_transformed : ndarray
-        The original data projected on the `n_components` principal components.
-
-    """
-    data_centered = data - np.mean(data, axis=0)
-    U, S, V = np.linalg.svd(data_centered, full_matrices=False)
-    U = U[:, :n_components]
-    U *= np.sqrt(data_centered.shape[0] - 1)
-    return U
+def expensive_computation(data, column_index=0):
+    """Simulate an expensive computation"""
+    time.sleep(5)
+    return data[column_index]
 
 
 rng = np.random.RandomState(42)
-data = rng.randn(int(1e8), 10)
+data = rng.randn(int(1e5), 10)
 tic = time.time()
-data_trans = pca(data)
+data_trans = expensive_computation(data)
 toc = time.time()
 
-print('\nPCA took {:.2f} s to compute.'.format(toc - tic))
+print('\nOur function took {:.2f} s to compute.'.format(toc - tic))
 print('\nThe transformed data are:\n {}'.format(data_trans))
 
 ###############################################################################
 # Caching the result of a function avoiding recomputing
 ###############################################################################
 # 
-# In the case that we would need to call ``pca`` function several time with
-# the same input data, it is beneficial to avoid recomputing the same results
-# over and over since that this function is time consuming. We can use
-# :class:`joblib.Memory` to avoid such recomputing. A :class:`joblib.Memory`
-# instance can be created by passing a directory in which we want to store the
-# results.
+# In the case that we would need to call our function several time with the
+# same input data, it is beneficial to avoid recomputing the same results over
+# and over since it is expensive. We can use :class:`joblib.Memory` to avoid
+# such recomputing. A :class:`joblib.Memory` instance can be created by passing
+# a directory in which we want to store the results.
 
 from joblib import Memory
-cachedir = './pca_caching'
+cachedir = './cachedir'
 memory = Memory(cachedir=cachedir, verbose=0)
 
 
-def pca_cached(data, n_components=2):
-    data_centered = data - np.mean(data, axis=0)
-    U, S, V = np.linalg.svd(data_centered, full_matrices=False)
-    U = U[:, :n_components]
-    U *= np.sqrt(data_centered.shape[0] - 1)
-    return U
+def expensive_computation_cached(data, column_index=0):
+    """Simulate an expensive computation"""
+    time.sleep(5)
+    return data[column_index]
 
 
-pca_cached = memory.cache(pca_cached)
+expensive_computation_cached = memory.cache(expensive_computation_cached)
 tic = time.time()
-data_trans = pca_cached(data)
+data_trans = expensive_computation_cached(data)
 toc = time.time()
 
-print('\nPCA took {:.2f} s to compute.'.format(toc - tic))
+print('\nOur function took {:.2f} s to compute.'.format(toc - tic))
 print('\nThe transformed data are:\n {}'.format(data_trans))
 
 ###############################################################################
@@ -93,10 +70,10 @@ print('\nThe transformed data are:\n {}'.format(data_trans))
 # results into the disk.
 
 tic = time.time()
-data_trans = pca_cached(data)
+data_trans = expensive_computation_cached(data)
 toc = time.time()
 
-print('\nPCA took {:.2f} s to compute.'.format(toc - tic))
+print('\nOur function took {:.2f} s to compute.'.format(toc - tic))
 print('\nThe transformed data are:\n {}'.format(data_trans))
 
 ###############################################################################
@@ -113,52 +90,28 @@ print('\nThe transformed data are:\n {}'.format(data_trans))
 # function and use it inside the class.
 
 
-def _pca_cached(data, n_components):
-    data_centered = data - np.mean(data, axis=0)
-    U, S, V = np.linalg.svd(data_centered, full_matrices=False)
-    U = U[:, :n_components]
-    U *= np.sqrt(data_centered.shape[0] - 1)
-    return U
+def _expensive_computation_cached(data, column):
+    time.sleep(5)
+    return data[column]
 
 
-class PCA(object):
-    """Principal components analysis.
+class Algorithm(object):
+    """A class which is using our pure function."""
 
-    Parameters
-    ----------
-    n_components : int, default=2
-        The number of components to keep.
-
-    """
-
-    def __init__(self, n_components=2):
-        self.n_components = n_components
+    def __init__(self, column=0):
+        self.column = column
 
     def transform(self, data):
-        """Transform the data.
-
-        Parameters
-        ----------
-        data : ndarray
-            The data to decompose.
-
-        Return
-        ------
-        data_transformed : ndarray
-            The original data projected on the `n_components` principal
-            components.
-
-        """
-        pca_cached = memory.cache(_pca_cached)
-        return pca_cached(data, self.n_components)
+        expensive_computation = memory.cache(_expensive_computation_cached)
+        return expensive_computation(data, self.column)
 
 
-transformer = PCA()
+transformer = Algorithm()
 tic = time.time()
 data_trans = transformer.transform(data)
 toc = time.time()
 
-print('\nPCA took {:.2f} s to compute.'.format(toc - tic))
+print('\nOur function took {:.2f} s to compute.'.format(toc - tic))
 print('\nThe transformed data are:\n {}'.format(data_trans))
 
 ###############################################################################
@@ -167,7 +120,7 @@ tic = time.time()
 data_trans = transformer.transform(data)
 toc = time.time()
 
-print('\nPCA took {:.2f} s to compute.'.format(toc - tic))
+print('\nOur function took {:.2f} s to compute.'.format(toc - tic))
 print('\nThe transformed data are:\n {}'.format(data_trans))
 
 ###############################################################################
