c9f58cba5955f3258b2c8b725567a74ab2c6da03
Elizabeth Sander <liz.sander91@gmail.com>
2018-01-26 03:35:29 -0600

    BUG stop numpy arrays hashing to same memmap (#565)
    * BUG add object id to pkl name for memmapping
    
    * BUG clarify test
    
    * TST add with_numpy decorator
    
    * TST skip win32 for memmap test
    
    * TST update test skip reason
    
    * don't use id for 'r' or 'c' mmap_mode
    
    * STY PEP8 fix

--- joblib/test/test_memmapping.py
+++ joblib/test/test_memmapping.py
@@ -7,8 +7,9 @@ from joblib.test.common import setup_autokill
 from joblib.test.common import teardown_autokill
 from joblib.test.common import with_multiprocessing
 from joblib.test.common import with_dev_shm
-from joblib.testing import raises, parametrize
+from joblib.testing import raises, parametrize, skipif
 from joblib.backports import make_memmap
+from joblib.parallel import Parallel, delayed
 
 from joblib.pool import MemmappingPool
 from joblib.executor import _TestingMemmappingExecutor
@@ -545,3 +546,21 @@ def test_pool_get_temp_dir(tmpdir):
     if sys.platform.startswith('win'):
         assert shared_mem is False
     assert pool_folder.endswith(pool_folder_name)
+
+
+@with_numpy
+@skipif(sys.platform == 'win32', reason='This test fails with a '
+        'PermissionError on Windows')
+@parametrize("mmap_mode", ["r+", "w+"])
+def test_numpy_arrays_use_different_memory(mmap_mode):
+    def func(arr, value):
+        arr[:] = value
+        return arr
+
+    arrays = [np.zeros((10, 10), dtype='float64') for i in range(10)]
+
+    results = Parallel(mmap_mode=mmap_mode, max_nbytes=0, n_jobs=2)(
+        delayed(func)(arr, i) for i, arr in enumerate(arrays))
+
+    for i, arr in enumerate(results):
+        np.testing.assert_array_equal(arr, i)
