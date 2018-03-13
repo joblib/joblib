d284f64a24397c01cebcdd0f8c14c2d603fa5200
Olivier Grisel <olivier.grisel@ensta.org>
2018-03-13 11:09:36 +0100

    TST run scikit-learn tests on CI

--- continuous_integration/travis/test_script.sh
+++ continuous_integration/travis/test_script.sh
@@ -14,3 +14,14 @@ if [[ "$SKIP_TESTS" != "true" ]]; then
     fi
     make
 fi
+
+if [[ "$SKLEARN_TESTS" == "true" ]]; then
+    export BUILD_ROOT="$HOME/build"
+    conda install --yes numpy scipy cython
+    git clone --depth=1 https://github.com/scikit-learn/scikit-learn.git \
+        $BUILD_ROOT/scikit-learn/scikit-learn
+    cd $BUILD_ROOT/scikit-learn/scikit-learn/sklearn/externals
+    bash copy_joblib.sh $BUILD_ROOT/joblib/joblib
+    cd $BUILD_ROOT/scikit-learn/scikit-learn
+    make
+fi
