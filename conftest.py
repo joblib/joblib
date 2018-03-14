00958fee44a92f7cae11abf2afb5b384d96413ce
Loïc Estève <loic.esteve@ymail.com>
2018-03-14 17:15:28 +0100

    Skip rst doctests if multiprocessing disabled

--- conftest.py
+++ conftest.py
@@ -3,17 +3,23 @@ from distutils.version import LooseVersion
 import pytest
 from _pytest.doctest import DoctestItem
 
+from joblib.parallel import mp
+
 
 def pytest_collection_modifyitems(config, items):
-    # numpy changed the str/repr formatting of numpy arrays in 1.14. We want to
-    # run doctests only for numpy >= 1.14.
     skip_doctests = True
-    try:
-        import numpy as np
-        if LooseVersion(np.__version__) >= LooseVersion('1.14'):
-            skip_doctests = False
-    except ImportError:
-        pass
+
+    # We do not want to run the doctests if multiprocessing is disabled
+    # e.g. via the JOBLIB_MULTIPROCESSING env variable
+    if mp is not None:
+        try:
+            # numpy changed the str/repr formatting of numpy arrays in 1.14.
+            # We want to run doctests only for numpy >= 1.14.
+            import numpy as np
+            if LooseVersion(np.__version__) >= LooseVersion('1.14'):
+                skip_doctests = False
+        except ImportError:
+            pass
 
     if skip_doctests:
         skip_marker = pytest.mark.skip(
