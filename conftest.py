e035061c155e35761434a43021b50f3b3076eb62
Thomas Moreau <toma.moral@gmail.com>
2018-04-12 07:56:34 +0200

    TST increase the verbosity level in test
    - Display the multiprocessin and loky logging message

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from distutils.version import LooseVersion
 import pytest
 from _pytest.doctest import DoctestItem
 
+import logging
 from joblib.parallel import mp
 
 
@@ -28,3 +29,11 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if isinstance(item, DoctestItem):
                 item.add_marker(skip_marker)
+
+
+def pytest_configure(config):
+    """Setup multiprocessing logging for the tests"""
+    if mp is not None:
+        log = mp.util.log_to_stderr(logging.DEBUG)
+        log.handlers[0].setFormatter(logging.Formatter(
+            '[%(levelname)s:%(processName)s:%(threadName)s] %(message)s'))
