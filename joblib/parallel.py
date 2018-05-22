5f99d12cc22beedb7446d5332174f815b83ba35a
Thomas Moreau <toma.moral@gmail.com>
2018-05-22 16:26:21 +0200

    CLN some cleaning of the code

--- joblib/parallel.py
+++ joblib/parallel.py
@@ -193,7 +193,8 @@ class BatchedCalls(object):
     def _wrap_non_picklable_objects(obj, pickle_cache):
         need_wrap = "__main__" in getattr(obj, "__module__", "")
         if callable(obj):
-            # Need wrap if the object is a local function
+            # Need wrap if the object is a function defined in a local scope of
+            # another function.
             func_code = getattr(obj, "__code__", "")
             need_wrap |= getattr(func_code, "co_flags", 0) & inspect.CO_NESTED
 
@@ -201,13 +202,14 @@ class BatchedCalls(object):
             func_name = getattr(obj, "__name__", "")
             need_wrap |= "<lambda>" in func_name
 
-        if need_wrap:
-            if obj in pickle_cache:
-                return pickle_cache[obj]
+        if not need_wrap:
+            return obj
+
+        wrapped_obj = pickle_cache.get(obj)
+        if wrapped_obj is None:
             wrapped_obj = CloudpickledObjectWrapper(obj)
             pickle_cache[obj] = wrapped_obj
-            return wrapped_obj
-        return obj
+        return wrapped_obj
 
     def __getstate__(self):
         items = [(self._wrap_non_picklable_objects(func, self._pickle_cache),
