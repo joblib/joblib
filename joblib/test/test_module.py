d4d6a13243eee538ca669af4a7f03efbb80b07c5
Thomas Moreau <thomas.moreau.2010@gmail.com>
2018-08-27 09:05:20 +0200

    Fix default context handling (#754)

---
+++ joblib/test/test_module.py
@@ -0,0 +1,21 @@
+import sys
+import joblib
+import pytest
+from joblib.testing import check_subprocess_call
+
+
+def test_version():
+    assert hasattr(joblib, '__version__'), (
+        "There are no __version__ argument on the joblib module")
+
+
+@pytest.mark.skipif(sys.version_info < (3, 3), reason="Need python3.3+")
+def test_import():
+    # check that importing joblib does not implicitly set the global
+    # start_method for multiprocessing.
+    code = """if True:
+        import joblib
+        import multiprocessing as mp
+        mp.set_start_method("loky")
+    """
+    check_subprocess_call([sys.executable, '-c', code])
