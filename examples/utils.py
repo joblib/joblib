781339fc9d65fc442a068296811e8d115b89d0ee
tomMoral <thomas.moreau.2010@gmail.com>
2018-11-01 14:11:54 +0100

    FIX missing file for doc

---
+++ examples/utils.py
@@ -0,0 +1,12 @@
+# Module to import functions from in examples for multiprocessing backend
+import numpy as np
+
+
+def stochastic_function_seeded(max_value, random_state):
+    rng = np.random.RandomState(random_state)
+    return rng.randint(max_value, size=5)
+
+
+def stochastic_function(max_value):
+    """Randomly generate integer up to a maximum value."""
+    return np.random.randint(max_value, size=5)
