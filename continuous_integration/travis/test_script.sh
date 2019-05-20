01f71ea4592d9d0408fb44439a5cc82e7923b318
Olivier Grisel <olivier.grisel@ensta.org>
2019-05-20 14:51:10 +0200

    MAINT test against the latest stable scikit-learn version

--- continuous_integration/travis/test_script.sh
+++ continuous_integration/travis/test_script.sh
@@ -20,13 +20,9 @@ if [[ "$SKIP_TESTS" != "true" ]]; then
 fi
 
 if [[ "$SKLEARN_TESTS" == "true" ]]; then
-    # Install scikit-learn from conda, patch it to use this version of joblib
-    # and run the scikit-learn tests with pytest. The versions of scikit-learn
-    # and scipy are frozen to avoid instabilities in the API (see issue #684)
-    # that should not be tested here.
-    conda install --yes scikit-learn==0.20.3 scipy==1.2.1
-    export SKLEARN=`python -c "import sklearn; print(sklearn.__path__[0])"`
-    cp $TRAVIS_BUILD_DIR/continuous_integration/travis/copy_joblib.sh $SKLEARN/externals
-    (cd $SKLEARN/externals && bash copy_joblib.sh $TRAVIS_BUILD_DIR)
-    pytest -vl $SKLEARN
+    # Install scikit-learn from conda and test against the installed
+    # development version of joblib.
+    conda install --yes scikit-learn scipy==1.2.1
+    python -c "import sklearn; print('Testing scikit-learn', sklearn.__version__)"
+    pytest -vl --pyargs sklearn
 fi
