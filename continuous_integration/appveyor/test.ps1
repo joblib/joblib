fd7d6e11ee120849d27f7596b96c011530dff07e
Pierre Glaser <pierreglaser@msn.com>
2019-06-12 14:38:05 +0200

    --amend

--- continuous_integration/appveyor/test.ps1
+++ continuous_integration/appveyor/test.ps1
@@ -1,2 +1,2 @@
-pytest joblib --cov joblib -vl --timeout 30 --ignore joblib/externals
+pytest joblib --cov joblib -vl --timeout 30 --ignore joblib/externals -k test_simple_parallel
 exit $LastExitCode
