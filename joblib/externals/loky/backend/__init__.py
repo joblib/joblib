8aa38c5d33906cd39860bac613f9da1c910e9643
GriffinJ <jrgriffiniii@gmail.com>
2020-01-26 12:57:46 -0500

    Handling cases where loky support is blocked for platforms which don't have a functioning sem_open implementation; Moving cloudpickle_wrapper (this is not dependent upon loky)

--- joblib/externals/loky/backend/__init__.py
+++ joblib/externals/loky/backend/__init__.py
@@ -9,8 +9,13 @@ if sys.version_info > (3, 4):
         name = '/loky-%i-%s' % (os.getpid(), next(synchronize.SemLock._rand))
         return name
 
-    # monkey patch the name creation for multiprocessing
-    from multiprocessing import synchronize
-    synchronize.SemLock._make_name = staticmethod(_make_name)
+    # Handle cases where semaphores are not supported
+    try:
+        # monkey patch the name creation for multiprocessing
+        from multiprocessing import synchronize
+        synchronize.SemLock._make_name = staticmethod(_make_name)
+    except ImportError as import_error:
+        # sys.stderr.write(f"Disabling loky support: {import_error}")
+        pass
 
 __all__ = ["get_context"]
--- joblib/parallel.py
+++ joblib/parallel.py
@@ -28,7 +28,13 @@ from ._parallel_backends import (FallbackToBackend, MultiprocessingBackend,
                                  LokyBackend)
 from ._compat import _basestring
 from .externals.cloudpickle import dumps, loads
-from .externals import loky
+
+DEFAULT_BACKEND = 'loky'
+# Don't default to loky if semaphores are not supported
+try:
+    from .externals import loky
+except ImportError:
+    DEFAULT_BACKEND = 'threading'
 
 # Make sure that those two classes are part of the public joblib.parallel API
 # so that 3rd party backend implementers can import them from here.
@@ -48,7 +54,6 @@ BACKENDS = {
 }
 # name of the backend used by default by Parallel outside of any context
 # managed by ``parallel_backend``.
-DEFAULT_BACKEND = 'loky'
 DEFAULT_N_JOBS = 1
 DEFAULT_THREAD_BACKEND = 'threading'
 
