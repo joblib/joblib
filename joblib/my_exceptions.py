58b38b3c2deb54a1327710e53d2228adac158323
Pierre Glaser <pierreglaser@msn.com>
2020-04-21 15:39:42 +0200

    Restore deleted joblib Exceptions; deprecate them

--- joblib/my_exceptions.py
+++ joblib/my_exceptions.py
@@ -1,5 +1,29 @@
+from warnings import warn
+from . import _deprecated_my_exceptions
+
+"""
+Exceptions
+"""
+# Author: Gael Varoquaux < gael dot varoquaux at normalesup dot org >
+# Copyright: 2010, Gael Varoquaux
+# License: BSD 3 clause
+
+_deprecated_names = [
+    name for name in dir(_deprecated_my_exceptions) if not
+    name.startswith("__")
+]
+
+
+def __getattr__(name):
+    if not name.startswith("__") and name in _deprecated_names:
+        warn(f"{name} is deprecated and will be removed from joblib in 0.16")
+        return getattr(_deprecated_my_exceptions, name)
+    raise AttributeError
+
+
 class WorkerInterrupt(Exception):
     """ An exception that is not KeyboardInterrupt to allow subprocesses
         to be interrupted.
     """
+
     pass
