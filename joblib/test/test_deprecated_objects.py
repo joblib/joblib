941b1f4b99f4e5c6cd55c9b5b23d1988ffbea925
Pierre Glaser <pierreglaser@msn.com>
2020-04-21 15:55:02 +0200

    also check deprecated object identity

--- joblib/test/test_deprecated_objects.py
+++ joblib/test/test_deprecated_objects.py
@@ -3,14 +3,17 @@ Tests making sure that deprecated objects properly raise a deprecation warning
 when imported/created.
 """
 from joblib import my_exceptions
-from joblib.my_exceptions import _deprecated_names
+from joblib import _deprecated_my_exceptions
+from joblib.my_exceptions import _deprecated_names as _deprecated_exceptions
 
 
 def test_deprecated_joblib_exceptions(capsys):
-    assert 'JoblibException' in _deprecated_names
-    for name in _deprecated_names:
+    assert 'JoblibException' in _deprecated_exceptions
+    for name in _deprecated_exceptions:
+        obj = getattr(my_exceptions, name)
+        assert obj is getattr(_deprecated_my_exceptions, name)
+
         msg = (f'UserWarning: {name} is deprecated and will be removed from '
                f'joblib in 0.16')
-        _ = getattr(my_exceptions, name)
         out, err = capsys.readouterr()
         assert msg in err
