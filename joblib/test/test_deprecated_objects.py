7ac6536f53fcce3eecd776b1739ffa848512af63
Pierre Glaser <pierreglaser@msn.com>
2020-04-21 15:59:33 +0200

    test deprecation warning for format_stack

--- joblib/test/test_deprecated_objects.py
+++ joblib/test/test_deprecated_objects.py
@@ -6,6 +6,10 @@ from joblib import my_exceptions
 from joblib import _deprecated_my_exceptions
 from joblib.my_exceptions import _deprecated_names as _deprecated_exceptions
 
+from joblib import format_stack
+from joblib import _deprecated_format_stack
+from joblib.format_stack import _deprecated_names as _deprecated_format_utils
+
 
 def test_deprecated_joblib_exceptions(capsys):
     assert 'JoblibException' in _deprecated_exceptions
@@ -16,4 +20,17 @@ def test_deprecated_joblib_exceptions(capsys):
         msg = (f'UserWarning: {name} is deprecated and will be removed from '
                f'joblib in 0.16')
         out, err = capsys.readouterr()
+        assert msg in err
+
+
+def test_deprecated_formatting_utilities(capsys):
+    assert 'safe_repr' in _deprecated_format_utils
+    assert 'eq_repr' in _deprecated_format_utils
+    for name in _deprecated_format_utils:
+        obj = getattr(format_stack, name)
+        assert obj is getattr(_deprecated_format_stack, name)
+
+        out, err = capsys.readouterr()
+        msg = (f'UserWarning: {name} is deprecated and will be removed from '
+               f'joblib in 0.16')
         assert msg in err
