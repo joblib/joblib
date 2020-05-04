58b6cb60f485480b63344a615976629da88264dc
Pierre Glaser <pierreglaser@msn.com>
2020-05-04 17:14:01 +0200

    fixup! typos

--- joblib/_memmapping_reducer.py
+++ joblib/_memmapping_reducer.py
@@ -78,12 +78,12 @@ def unlink_file(filename):
     """Wrapper around os.unlink with a retry mechanism.
 
     The retry mechanism has been implemented primarily to overcome a race
-    condition happening during the finalizer of a memmap: when a process
-    holding the last reference to a mmap-backed np.array is about to delete
-    this array (and close the reference), it sends a maybe_unlink request to
-    the resource_tracker. This request can be processed faster than it takes
-    for the last reference of the memmap to be closed, yielding (on windows) a
-    PermissionError in the resource_tracker loop.
+    condition happening during the finalizer of a np.memmap: when a process
+    holding the last reference to a mmap-backed np.memmap/np.array is about to
+    delete this array (and close the reference), it sends a maybe_unlink
+    request to the resource_tracker. This request can be processed faster than
+    it takes for the last reference of the memmap to be closed, yielding (on
+    Windows) a PermissionError in the resource_tracker loop.
     """
     NUM_RETRIES = 10
     for retry_no in range(1, NUM_RETRIES + 1):
