8cba4a59e49115f1c36412f50edd85116c5044cc
Pierre Glaser <pierreglaser@msn.com>
2020-05-06 00:45:24 +0200

    subclass loky reusable executor

--- joblib/executor.py
+++ joblib/executor.py
@@ -11,41 +11,51 @@ copy between the parent and child processes.
 import random
 from ._memmapping_reducer import get_memmapping_reducers
 from ._memmapping_reducer import TemporaryResourcesManagerMixin
-from .externals.loky.reusable_executor import get_reusable_executor
+from .externals.loky.reusable_executor import _ReusablePoolExecutor
 
 
 _executor_args = None
 
 
-def get_memmapping_executor(n_jobs, timeout=300, initializer=None, initargs=(),
-                            env=None, **backend_args):
-    """Factory for ReusableExecutor with automatic memmapping for large numpy
-    arrays.
-    """
-    global _executor_args
-    # Check if we can reuse the executor here instead of deferring the test to
-    # loky as the reducers are objects that changes at each call.
-    executor_args = backend_args.copy()
-    executor_args.update(env if env else {})
-    executor_args.update(dict(
-        timeout=timeout, initializer=initializer, initargs=initargs))
-    reuse = _executor_args is None or _executor_args == executor_args
-    _executor_args = executor_args
+def get_memmapping_executor(n_jobs, **kwargs):
+    return MemmappingExecutor.get_memmapping_executor(n_jobs, **kwargs)
 
-    id_executor = random.randint(0, int(1e10))
-    job_reducers, result_reducers, temp_folder = get_memmapping_reducers(
-        id_executor, unlink_on_gc_collect=True, **backend_args)
-    _executor = get_reusable_executor(n_jobs, job_reducers=job_reducers,
-                                      result_reducers=result_reducers,
-                                      reuse=reuse, timeout=timeout,
-                                      initializer=initializer,
-                                      initargs=initargs, env=env)
-    # If executor doesn't have a _temp_folder, it means it is a new executor
-    # and the reducers have not been used. Else, the previous reducers are used
-    # and we should not change this attribute.
-    if not hasattr(_executor, "_temp_folder"):
-        _executor._temp_folder = temp_folder
-    return _executor
+
+class MemmappingExecutor(
+        _ReusablePoolExecutor, TemporaryResourcesManagerMixin
+):
+
+    @classmethod
+    def get_memmapping_executor(cls, n_jobs, timeout=300, initializer=None,
+                                initargs=(), env=None, **backend_args):
+        """Factory for ReusableExecutor with automatic memmapping for large numpy
+        arrays.
+        """
+        global _executor_args
+        # Check if we can reuse the executor here instead of deferring the test
+        # to loky as the reducers are objects that changes at each call.
+        executor_args = backend_args.copy()
+        executor_args.update(env if env else {})
+        executor_args.update(dict(
+            timeout=timeout, initializer=initializer, initargs=initargs))
+        reuse = _executor_args is None or _executor_args == executor_args
+        _executor_args = executor_args
+
+        id_executor = random.randint(0, int(1e10))
+        job_reducers, result_reducers, temp_folder = get_memmapping_reducers(
+            id_executor, unlink_on_gc_collect=True, **backend_args)
+        _executor, _ = super().get_reusable_executor(
+            n_jobs, job_reducers=job_reducers, result_reducers=result_reducers,
+            reuse=reuse, timeout=timeout, initializer=initializer,
+            initargs=initargs, env=env
+        )
+        # If executor doesn't have a _temp_folder, it means it is a new
+        # executor and the reducers have not been used. Else, the previous
+        # reducers are used and we should not change this attribute.
+        if not hasattr(_executor, "_temp_folder"):
+            _executor._temp_folder = temp_folder
+
+        return _executor
 
 
 class _TestingMemmappingExecutor(TemporaryResourcesManagerMixin):
