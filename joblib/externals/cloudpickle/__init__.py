adfe0c8f7b4459bd1f3c35a9caa4b62bfd8ab982
Olivier Grisel <olivier.grisel@gmail.com>
2020-07-24 14:17:40 +0200

    MAINT cloudpickle 1.5.0 (#1090)

--- joblib/externals/cloudpickle/__init__.py
+++ joblib/externals/cloudpickle/__init__.py
@@ -1,11 +1,7 @@
 from __future__ import absolute_import
 
-import sys
-import pickle
 
+from .cloudpickle import *  # noqa
+from .cloudpickle_fast import CloudPickler, dumps, dump  # noqa
 
-from .cloudpickle import *
-if sys.version_info[:2] >= (3, 8):
-    from .cloudpickle_fast import CloudPickler, dumps, dump
-
-__version__ = '1.4.1'
+__version__ = '1.5.0'
