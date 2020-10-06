f73a32d341da9f4fcd212186ffc4da7f0724459d
Gael Varoquaux <gael.varoquaux@normalesup.org>
2020-10-06 11:49:41 +0200

    skip doctest

--- joblib/parallel.py
+++ joblib/parallel.py
@@ -146,10 +146,10 @@ class parallel_backend(object):
     the workload to a cluster of nodes. To use the 'ray' joblib backend add
     the following lines::
 
-     >>> from ray.util.joblib import register_ray
-     >>> register_ray()
-     >>> with parallel_backend("ray"):
-     ...     print(Parallel()(delayed(neg)(i + 1) for i in range(5)))
+     >>> from ray.util.joblib import register_ray # doctest: +SKIP
+     >>> register_ray() # doctest: +SKIP
+     >>> with parallel_backend("ray"): # doctest: +SKIP
+     ...     print(Parallel()(delayed(neg)(i + 1) for i in range(5))) # doctest: +SKIP
      [-1, -2, -3, -4, -5]
 
     Alternatively the backend can be passed directly as an instance.
