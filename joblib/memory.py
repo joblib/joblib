64636940141f08475b1032a4d61ad15c03c98736
Loïc Estève <loic.esteve@ymail.com>
2021-03-18 10:34:57 +0100

    Use text rather than fancy sphinx because my CSS skills are not good enough to make it pretty.

--- joblib/memory.py
+++ joblib/memory.py
@@ -884,11 +884,8 @@ class Memory(Logger):
         bytes_limit: int, optional
             Limit in bytes of the size of the cache. By default, the size of
             the cache is unlimited.
-
-            .. note::
-
-               You need to call :meth:`joblib.Memory.reduce_size` to actually
-               reduce the cache size to be less than ``bytes_limit``.
+            Note: You need to call :meth:`joblib.Memory.reduce_size` to
+            actually reduce the cache size to be less than ``bytes_limit``.
 
         backend_options: dict, optional
             Contains a dictionnary of named parameters used to configure
