095e7b2fe4712d2b9aee364ac3e90683dc8ab350
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-10 14:01:04 +0200

    Vendor up

--- joblib/externals/loky/backend/utils.py
+++ joblib/externals/loky/backend/utils.py
@@ -117,7 +117,7 @@ def _recursive_terminate(pid):
 
 
 def get_exitcodes_terminated_worker(processes):
-    """Return a formatted string with the exitcodes of terminated workers.
+    """Return a formated string with the exitcodes of terminated workers.
 
     If necessary, wait (up to .25s) for the system to correctly set the
     exitcode of one terminated worker.
