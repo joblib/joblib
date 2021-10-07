8ada0c30b86597a61e74d9bfa4055f89c043f4d6
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-10-07 18:35:08 +0200

    BUG/CI: avoid failures on unreadable files

--- continuous_integration/run_tests.sh
+++ continuous_integration/run_tests.sh
@@ -40,7 +40,7 @@ fi
 
 if [[ "$SKIP_TESTS" != "true" && "$COVERAGE" == "true" ]]; then
     echo "Deleting empty coverage files:"
-    find . -name ".coverage.*" -size  0 -print -delete
+    find . ! -readable -prune -name ".coverage.*" -size  0 -print -delete
     echo "Combining .coverage.* files..."
     coverage combine --append  || echo "Found invalid coverage files."
     echo "Generating XML Coverage report..."
