cdce73b833ea17b0baa4ed3ba9668ebef35b5d44
Hood Chatham <roberthoodchatham@gmail.com>
2022-01-10 10:38:50 -0800

    Run formatter, update comments

--- joblib/test/test_missing_multiprocessing.py
+++ joblib/test/test_missing_multiprocessing.py
@@ -7,19 +7,18 @@ import os
 import subprocess
 import sys
 
+
 def test_missing_multiprocessing():
     """
     Test that import joblib works even if _multiprocessing is missing.
 
-    pytest has already imported everything from joblib, so the easiest way to
-    test importing it, we need to invoke a separate Python process. This also
-    makes it easy to ensure that we don't break other tests by importing a bad
-    `_multiprocessing` module.
+    pytest has already imported everything from joblib. The most reasonable way
+    to test importing joblib with modified environment is to invoke a separate
+    Python process. This also ensures that we don't break other tests by
+    importing a bad `_multiprocessing` module.
     """
     env = dict(os.environ)
     # For subprocess, use current sys.path with our custom version of
     # multiprocessing inserted.
     env["PYTHONPATH"] = ":".join(["./test/missing_multiprocessing"] + sys.path)
     subprocess.check_call([sys.executable, "-c", "import joblib"], env=env)
-
-
