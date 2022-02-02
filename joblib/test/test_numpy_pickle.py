282d953f49cd1e73049364b9022e2a4abff44807
Loïc Estève <loic.esteve@ymail.com>
2022-02-02 17:46:43 +0100

    test

--- joblib/test/test_numpy_pickle.py
+++ joblib/test/test_numpy_pickle.py
@@ -1056,3 +1056,45 @@ def test_lz4_compression_without_lz4(tmpdir):
     with raises(ValueError) as excinfo:
         numpy_pickle.dump(data, fname + '.lz4')
     excinfo.match(msg)
+
+
+@with_numpy
+@parametrize('protocol', range(0, pickle.HIGHEST_PROTOCOL + 1))
+def test_memmap_alignment_padding(tmpdir, protocol):
+    # Test that memmaped arrays returned by numpy.load are correctly aligned
+    fname = tmpdir.join('test.mmap').strpath
+
+    a = np.random.randn(2)
+    numpy_pickle.dump(a, fname, protocol=protocol)
+    memmap = numpy_pickle.load(fname, mmap_mode='r')
+    assert isinstance(memmap, np.memmap)
+    np.testing.assert_array_equal(a, memmap)
+    assert memmap.ctypes.data % 8 == 0
+    assert memmap.flags.aligned
+
+    l = [np.random.randn(2), np.random.randn(2),
+         np.random.randn(2), np.random.randn(2)]
+
+    numpy_pickle.dump(l, fname, protocol=protocol)
+    l_reloaded = numpy_pickle.load(fname, mmap_mode='r')
+
+    for idx, memmap in enumerate(l_reloaded):
+        assert isinstance(memmap, np.memmap)
+        np.testing.assert_array_equal(l[idx], memmap)
+        print("MODULO: {}".format(memmap.ctypes.data % 8))
+        assert memmap.ctypes.data % 8 == 0
+        assert memmap.flags.aligned
+
+    d = {'a1': np.random.randn(100),
+         'a2': np.random.randn(200),
+         'a3': np.random.randn(300),
+         'a4': np.random.randn(400)}
+
+    numpy_pickle.dump(d, fname, protocol=protocol)
+    d_reloaded = numpy_pickle.load(fname, mmap_mode='r')
+
+    for key, memmap in d_reloaded.items():
+        assert isinstance(memmap, np.memmap)
+        np.testing.assert_array_equal(d[key], memmap)
+        assert memmap.ctypes.data % 8 == 0
+        assert memmap.flags.aligned
