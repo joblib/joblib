44909ced421ff7117c281137d2cd8adf0abe75ef
Olivier Grisel <olivier.grisel@ensta.org>
2022-09-08 09:28:38 +0200

    cloudpickle 2.2.0

--- joblib/externals/cloudpickle/__init__.py
+++ joblib/externals/cloudpickle/__init__.py
@@ -5,4 +5,4 @@ from .cloudpickle_fast import CloudPickler, dumps, dump  # noqa
 # expose their Pickler subclass at top-level under the  "Pickler" name.
 Pickler = CloudPickler
 
-__version__ = '2.1.0'
+__version__ = '2.2.0'
