09085045cb14a1c0f8182d7f93a4b2c9b1ebfe59
KOLANICH <kolan_n@mail.ru>
2023-02-14 15:33:07 +0300

    Move the metadata into `setup.cfg`

--- setup.py
+++ setup.py
@@ -1,61 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import pathlib
-import joblib
 
-# Get the long description from the README file
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.rst').read_text(encoding='utf-8')
-
-
-setup(
-    name='joblib',
-    version=joblib.__version__,
-    author='Gael Varoquaux',
-    author_email='gael.varoquaux@normalesup.org',
-    url='https://joblib.readthedocs.io',
-    project_urls={
-        'Source': 'https://github.com/joblib/joblib',
-    },
-    license='BSD-3-Clause',
-    description="Lightweight pipelining with Python functions",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Utilities',
-        'Topic :: Software Development :: Libraries',
-    ],
-    platforms='any',
-    package_data={
-        'joblib.test': [
-            'data/*.gz',
-            'data/*.gzip',
-            'data/*.bz2',
-            'data/*.xz',
-            'data/*.lzma',
-            'data/*.pkl',
-            'data/*.npy',
-            'data/*.npy.z',
-        ]
-    },
-    packages=[
-        'joblib', 'joblib.test', 'joblib.test.data',
-        'joblib.externals', 'joblib.externals.cloudpickle',
-        'joblib.externals.loky', 'joblib.externals.loky.backend',
-    ],
-    python_requires='>=3.7',
-)
+if __name__ == "__main__":
+    setup()
