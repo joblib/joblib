db5cf46379b5d1255ea504829708b1d72cd7d1b8
Franck Charras <29153872+fcharras@users.noreply.github.com>
2023-04-24 11:09:04 +0200

    CLN make generator exit thread safe - warning suggestion
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- joblib/parallel.py
+++ joblib/parallel.py
@@ -39,6 +39,9 @@ from ._parallel_backends import AutoBatchingMixin  # noqa
 from ._parallel_backends import ParallelBackendBase  # noqa
 
 
+IS_PYPY = hasattr(sys, "pypy_version_info")
+
+
 BACKENDS = {
     'threading': ThreadingBackend,
     'sequential': SequentialBackend,
@@ -1281,6 +1284,15 @@ class Parallel(Logger):
             # the rest of the function does not call `_terminate_and_reset`
             # in finally.
             if dispatch_thread_id != threading.get_ident():
+                if not IS_PYPY:
+                    warnings.warn(
+                        "A generator produced by joblib.Parallel has been gc'ed in "
+                        "an unexpected thread. This behavior should not cause major "
+                        "issues but to make sure, please report this warning and "
+                        "your use case at https://github.com/joblib/joblib/issues "
+                        "so it can be investigated."
+                    )
+
                 detach_generator_exit = True
                 _parallel = self
 
