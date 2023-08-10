12b545a0dffe1331855c3e6f5667b75a5c7735a4
tommoral <thomas.moreau.2010@gmail.com>
2023-08-10 13:52:17 +0200

    FIX linting

--- joblib/test/test_parallel.py
+++ joblib/test/test_parallel.py
@@ -463,7 +463,7 @@ def test_error_in_task_iterator(backend):
         with raises(ValueError, match="Iterator Raising Error"):
             p(delayed(square)(i) for i in my_generator(raise_at=0))
 
-        # The error is raised when dispatching a new task after the 
+        # The error is raised when dispatching a new task after the
         # pre-dispatch (likely to happen in a different thread)
         with raises(ValueError, match="Iterator Raising Error"):
             p(delayed(square)(i) for i in my_generator(raise_at=5))
