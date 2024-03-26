52cc2861d78d60c7b9a55bbf01f082e4d9e8735d
Tobias Rahloff <rahloff@gmail.com>
2024-03-26 17:41:57 +0100

    fix linting issues

--- joblib/logger.py
+++ joblib/logger.py
@@ -1,12 +1,6 @@
 """
 Helpers for logging.
-
-This module needs much love to become useful.
 """
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# Copyright (c) 2008 Gael Varoquaux
-# License: BSD Style, 3 clauses.
 
 import time
 import sys
@@ -23,25 +17,29 @@ def _squeeze_time(t):
     This is the time it takes to stat files. This is needed to make results
     similar to timings under Unix, for tests.
     """
-    if sys.platform.startswith('win'):
-        return max(0, t - .1)
+    if sys.platform.startswith("win"):
+        return max(0, t - 0.1)
     else:
         return t
 
+
 def format_time(t):
     t = _squeeze_time(t)
-    return "%.1fs, %.1fmin" % (t, t / 60.)
+    return "%.1fs, %.1fmin" % (t, t / 60.0)
+
 
 def short_format_time(t):
     t = _squeeze_time(t)
     if t > 60:
-        return "%4.1fmin" % (t / 60.)
+        return "%4.1fmin" % (t / 60.0)
     else:
         return " %5.1fs" % (t)
 
+
 def pformat(obj, indent=0, depth=3):
-    if 'numpy' in sys.modules:
+    if "numpy" in sys.modules:
         import numpy as np
+
         print_options = np.get_printoptions()
         np.set_printoptions(precision=6, threshold=64, edgeitems=1)
     else:
@@ -50,6 +48,7 @@ def pformat(obj, indent=0, depth=3):
     if print_options:
         np.set_printoptions(**print_options)
     return out
+
 
 class Logger:
     """Base class for logging messages."""
@@ -64,14 +63,13 @@ class Logger:
             The namespace to log to. If None, defaults to joblib.
         """
         self.depth = depth
-        self._name = name if name else 'joblib'
+        self._name = name if name else "joblib"
         self._logger = logging.getLogger(self._name)
 
     def warn(self, msg):
         self._logger.warning("[%s]: %s", self, msg)
 
     def info(self, msg):
-        print(msg)
         self._logger.info("[%s]: %s", self, msg)
 
     def debug(self, msg):
@@ -81,44 +79,51 @@ class Logger:
         """Return the formatted representation of the object."""
         return pformat(obj, indent=indent, depth=self.depth)
 
+
 class PrintTime:
     """Print and log messages while keeping track of time."""
 
     def __init__(self, logfile=None, logdir=None):
         if logfile is not None and logdir is not None:
-            raise ValueError('Cannot specify both logfile and logdir')
+            raise ValueError("Cannot specify both logfile and logdir")
         self.last_time = time.time()
         self.start_time = self.last_time
         self.logfile = None
         if logdir is not None:
-            logfile = os.path.join(logdir, 'joblib.log')
+            logfile = os.path.join(logdir, "joblib.log")
         if logfile is not None:
             mkdirp(os.path.dirname(logfile))
             if os.path.exists(logfile):
                 # Rotate the logs
                 for i in range(8, 0, -1):
                     try:
-                        shutil.move(logfile + f'.{i}', logfile + f'.{i+1}')
+                        shutil.move(logfile + f".{i}", logfile + f".{i+1}")
                     except FileNotFoundError:
                         pass
                 try:
-                    shutil.copy(logfile, logfile + '.1')
+                    shutil.copy(logfile, logfile + ".1")
                 except FileNotFoundError:
                     pass
             self.logfile = logfile
 
-    def __call__(self, msg='', total=False):
-        """Print the time elapsed between the last call and the current call, with an optional message."""
+    def __call__(self, msg="", total=False):
+        """Print the time elapsed between the last call and the current call,
+        with an optional message.
+        """
         if not total:
             time_lapse = time.time() - self.last_time
             full_msg = "%s: %s" % (msg, format_time(time_lapse))
         else:
             time_lapse = time.time() - self.start_time
-            full_msg = "%s: %.2fs, %.1f min" % (msg, time_lapse, time_lapse / 60)
+            full_msg = "%s: %.2fs, %.1f min" % (
+                msg,
+                time_lapse,
+                time_lapse / 60
+            )
         print(full_msg, file=sys.stderr)
         if self.logfile is not None:
             try:
-                with open(self.logfile, 'a') as f:
+                with open(self.logfile, "a") as f:
                     print(full_msg, file=f)
             except IOError:
                 # Multiprocessing writing to files can create race conditions.
