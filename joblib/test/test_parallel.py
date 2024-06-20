ef1d91bf6100ea4e8bd74f78950ca4d8097be586
Loïc Estève <loic.esteve@ymail.com>
2024-06-20 05:30:23 +0200

    tweakg

--- joblib/test/test_parallel.py
+++ joblib/test/test_parallel.py
@@ -212,8 +212,8 @@ def _assert_warning_nested(backend, inner_n_jobs, expected):
                 'backed parallel loops cannot' in each.args[0]
                 for each in warninfo
             )
-            # With free-threaded Python, when the outer backend is threading, we might
-            # see more that one warning
+            # With free-threaded Python, when the outer backend is threading,
+            # we might see more that one warning
             warnings_have_the_right_length = (
                 len(warninfo) >= 1 if IS_GIL_DISABLED
                 else len(warninfo) == 1)
