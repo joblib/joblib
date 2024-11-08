cdfe72559b28d73cb6ca4b813d904b1d738fdffc
tommoral <thomas.moreau.2010@gmail.com>
2024-11-08 10:58:44 +0100

    FIX tentative to call conda init

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -11,6 +11,7 @@
 set -xe
 
 create_new_conda_env() {
+    conda init bash
     conda config --set solver libmamba
     to_install="python=$PYTHON_VERSION pip pytest $EXTRA_CONDA_PACKAGES"
     conda create -n testenv --yes -c conda-forge $to_install
