50f8891894a1d7e158e0198f57c874ae578ba154
LunarLanding <4441338+LunarLanding@users.noreply.github.com>
2025-02-17 10:41:33 +0100

    Include hostname in temporary file name
    Closes https://github.com/joblib/joblib/issues/1610

--- joblib/_store_backends.py
+++ joblib/_store_backends.py
@@ -12,6 +12,7 @@ import warnings
 import collections
 import operator
 import threading
+import socket
 from abc import ABCMeta, abstractmethod
 
 from .backports import concurrency_safe_rename
@@ -30,9 +31,10 @@ class CacheWarning(Warning):
 
 def concurrency_safe_write(object_to_write, filename, write_func):
     """Writes an object into a unique file in a concurrency-safe way."""
+    hostname = socket.getfqdn()
     thread_id = id(threading.current_thread())
-    temporary_filename = '{}.thread-{}-pid-{}'.format(
-        filename, thread_id, os.getpid())
+    temporary_filename = '{}.hostname-{hostname}-thread-{}-pid-{}'.format(
+        filename, hostname, thread_id, os.getpid())
     write_func(object_to_write, temporary_filename)
 
     return temporary_filename
