b2e4c1bc710f21bf0eb8d02cfceb658a7e12c54b
LunarLanding <4441338+LunarLanding@users.noreply.github.com>
2025-02-19 07:49:41 +0100

    Update joblib/_store_backends.py
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- joblib/_store_backends.py
+++ joblib/_store_backends.py
@@ -33,7 +33,7 @@ def concurrency_safe_write(object_to_write, filename, write_func):
     """Writes an object into a unique file in a concurrency-safe way."""
     hostname = socket.getfqdn()
     thread_id = id(threading.current_thread())
-    temporary_filename = '{}.hostname-{hostname}-thread-{}-pid-{}'.format(
+    temporary_filename = '{}.hostname-{}-thread-{}-pid-{}'.format(
         filename, hostname, thread_id, os.getpid())
     write_func(object_to_write, temporary_filename)
 
