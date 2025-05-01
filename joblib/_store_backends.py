80b12c85a574deba2d77ac923afda868a889c64a
tommoral <thomas.moreau.2010@gmail.com>
2025-05-01 19:03:38 +0200

    ENH use quote to sanitize hostname

--- joblib/_store_backends.py
+++ joblib/_store_backends.py
@@ -8,12 +8,13 @@ import os
 import os.path
 import re
 import shutil
+import socket
+import threading
 import time
 import warnings
-import threading
-import socket
 from abc import ABCMeta, abstractmethod
 from pickle import PicklingError
+from urllib.parse import quote
 
 from . import numpy_pickle
 from .backports import concurrency_safe_rename
@@ -31,10 +32,12 @@ class CacheWarning(Warning):
 
 def concurrency_safe_write(object_to_write, filename, write_func):
     """Writes an object into a unique file in a concurrency-safe way."""
-    hostname = socket.getfqdn()
+    hostname = quote(socket.getfqdn())
     thread_id = id(threading.current_thread())
-    temporary_filename = '{}.hostname-{}-thread-{}-pid-{}'.format(
-        filename, hostname, thread_id, os.getpid())
+    temporary_filename = (
+        f"{filename}.hostname-{hostname}-thread-{thread_id}-pid-{os.getpid()}"
+    )
+
     write_func(object_to_write, temporary_filename)
 
     return temporary_filename
