0879215a313cd031423cc1f430e1ba5489bf52ae
tommoral <thomas.moreau.2010@gmail.com>
2025-05-23 16:36:09 +0200

    CLN tweak comment

--- joblib/_store_backends.py
+++ joblib/_store_backends.py
@@ -31,10 +31,11 @@ class CacheWarning(Warning):
 
 def concurrency_safe_write(object_to_write, filename, write_func):
     """Writes an object into a unique file in a concurrency-safe way."""
+    # Temporary name is composed of UUID, process_id and thread_id to avoid
+    # collisions due to concurrent write.
     # UUID is unique across nodes and time and help avoid collisions, even if
     # the cache folder is shared by several Python processes with the same pid and
     # thread id on different nodes of a cluster for instance.
-    # Add thread_id and process_id to avoid collisions due to concurrency
     thread_id = id(threading.current_thread())
     temporary_filename = f"{filename}.{uuid.uuid4().hex}-{os.getpid()}-{thread_id}"
 
