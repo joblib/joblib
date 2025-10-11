475977ed4ad8500abf084f3d8f1443753c9b33e7
Daniel Darabos <darabos.daniel@gmail.com>
2025-10-11 12:58:17 +0200

    Test func_inspect with lambdas and doctests.

--- joblib/test/test_func_inspect.py
+++ joblib/test/test_func_inspect.py
@@ -182,12 +182,19 @@ def test_func_inspect_errors():
     assert get_func_name("a".lower)[-1] == "lower"
     assert get_func_code("a".lower)[1:] == (None, -1)
     ff = lambda x: x  # noqa: E731
+    correct_line = ff.__code__.co_firstlineno
     assert get_func_name(ff, win_characters=False)[-1] == "<lambda>"
-    assert get_func_code(ff)[1] == __file__.replace(".pyc", ".py")
+    code, file, first_line = get_func_code(ff)
+    assert "lambda x: x" in code
+    assert file == __file__.replace(".pyc", ".py")
+    assert first_line == correct_line
     # Simulate a function defined in __main__
     ff.__module__ = "__main__"
     assert get_func_name(ff, win_characters=False)[-1] == "<lambda>"
-    assert get_func_code(ff)[1] == __file__.replace(".pyc", ".py")
+    code, file, first_line = get_func_code(ff)
+    assert "lambda x: x" in code
+    assert file == __file__.replace(".pyc", ".py")
+    assert first_line == correct_line
 
 
 def func_with_kwonly_args(a, b, *, kw1="kw1", kw2="kw2"):
