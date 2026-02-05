7f3a189f539ed19263141a4be4e7d12412c89959
Nanored4498 <nanored.mc@gmail.com>
2026-02-05 11:13:37 +0100

    move some packages to conda

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -15,33 +15,30 @@ create_new_conda_env() {
         PYTHON_VERSION=${PYTHON_VERSION/free-threaded-/}
         EXTRA_CONDA_PACKAGES="python-freethreading $EXTRA_CONDA_PACKAGES"
     fi
-    to_install="python=$PYTHON_VERSION pip pytest $EXTRA_CONDA_PACKAGES"
+    to_install="python=$PYTHON_VERSION pip \
+        pytest pytest-timeout pytest-asyncio threadpoolctl \
+        $EXTRA_CONDA_PACKAGES"
     conda create -n testenv --yes -c conda-forge $to_install
     conda activate testenv
 }
 
 create_new_conda_env
 
-# Install pytest timeout to fasten failure in deadlocking tests
-PIP_INSTALL_PACKAGES="pytest-timeout pytest-asyncio==0.21.1 threadpoolctl"
-
 if [ "$NO_NUMPY" != "true" ]; then
     # We want to ensure no memory copies are performed only when numpy is
     # installed. This also ensures that we don't keep a strong dependency on
     # memory_profiler.
-    PIP_INSTALL_PACKAGES="$PIP_INSTALL_PACKAGES memory_profiler numpy"
+    pip install memory_profiler numpy
     # We also want to ensure that joblib can be used with and
     # without lz4 compressor package installed.
     if [ "$NO_LZ4" != "true" ]; then
-        PIP_INSTALL_PACKAGES="$PIP_INSTALL_PACKAGES lz4"
+        pip install lz4
     fi
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
-    PIP_INSTALL_PACKAGES="$PIP_INSTALL_PACKAGES coverage pytest-cov"
+     pip install coverage pytest-cov
 fi
-
-pip install $PIP_INSTALL_PACKAGES
 
 if [[ "$NO_LZMA" == "true" ]]; then
     # Delete the LZMA module from the standard lib to make sure joblib has no
