95a5f180200c904545097368715d583ae7b1d85e
Nanored4498 <nanored.mc@gmail.com>
2026-02-05 14:41:04 +0100

    move installations from run_tests to install

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -11,11 +11,11 @@ conda config --set solver libmamba
 
 if [[ $PYTHON_VERSION == free-threaded* ]]; then
     PYTHON_VERSION=${PYTHON_VERSION/free-threaded-/}
-    EXTRA_CONDA_PACKAGES="python-freethreading pip $EXTRA_CONDA_PACKAGES"
+    EXTRA_CONDA_PACKAGES="python-freethreading $EXTRA_CONDA_PACKAGES"
     FREE_THREADED="true"
 fi
 
-to_install="python=$PYTHON_VERSION \
+to_install="python=$PYTHON_VERSION pip \
     pytest pytest-timeout pytest-asyncio \
     $EXTRA_CONDA_PACKAGES"
 
@@ -38,10 +38,17 @@ if [[ $NO_LZ4 != "true" && $FREE_THREADED != "true" ]]; then
     to_install="$to_install lz4"
 fi
 
-if [[ $CYTHON == "true" ]]; then
+# sklearn_tests requires scipy
+if [[ $SKLEARN_TESTS == "true" ]]; then
+    to_install="$to_install scipy"
+fi
+
+# sklearn_tests requires cython
+if [[ $CYTHON == "true" || $SKLEARN_TESTS != "true" ]]; then
     to_install="$to_install cython"
 fi
 
+# We do not use coverage for sklearn_tests
 if [[ $COVERAGE == "true" && $SKLEARN_TESTS != "true" ]]; then
     to_install="$to_install coverage pytest-cov"
 fi
@@ -55,15 +62,20 @@ if [[ $NO_LZ4 != "true" && $FREE_THREADED == "true" ]]; then
     pip install lz4
 fi
 
+# Install the nightly build of scikit-learn
+if [[ $SKLEARN_TESTS == "true" ]]; then
+    pip install --pre --extra-index https://pypi.anaconda.org/scientific-python-nightly-wheels/simple scikit-learn
+fi
+
+# Delete the LZMA module from the standard lib to make sure joblib has no
+# hard dependency on it:
 if [[ "$NO_LZMA" == "true" ]]; then
-    # Delete the LZMA module from the standard lib to make sure joblib has no
-    # hard dependency on it:
     LZMA_PATH=`python -c "import lzma; print(lzma.__file__)"`
     echo "Deleting $LZMA_PATH..."
     rm $LZMA_PATH
 fi
 
-if [[ "$CYTHON" == "true" ]]; then
+if [[ $CYTHON == "true" && $SKLEARN_TESTS != "true" ]]; then
     cd joblib/test/_openmp_test_helper
     python setup.py build_ext -i
     cd ../../..
