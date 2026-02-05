16fbacbf25c87c7f6169629881d341907922fdf0
Nanored4498 <nanored.mc@gmail.com>
2026-02-05 16:58:25 +0100

    change numpy versions

--- continuous_integration/install_old.sh
+++ continuous_integration/install_old.sh
@@ -9,18 +9,18 @@ set -xe
 
 conda config --set solver libmamba
 
-conda create -n py27-np16 --yes -c conda-forge python=2.7 pip
-conda activate py27-np16
-pip install numpy==1.6 joblib==0.8.4 lz4
-conda create -n py27-np18 --yes -c conda-forge python=2.7 pip
-conda activate py27-np18
-pip install numpy==1.8 joblib==0.9.2 lz4
-conda create -n py35-np110 --yes -c conda-forge python=3.5 pip
-conda activate py35-np110
-pip install numpy==1.10 joblib==0.10.0 lz4
+conda create -n py27-np19 --yes -c conda-forge python=2.7 pip
+conda activate py27-np19
+pip install numpy==1.9 joblib==0.8.4 lz4
+conda create -n py27-np110 --yes -c conda-forge python=2.7 pip
+conda activate py27-np110
+pip install numpy==1.10 joblib==0.9.2 lz4
+conda create -n py35-np111 --yes -c conda-forge python=3.5 pip
+conda activate py35-np111
+pip install numpy==1.11 joblib==0.10.0 lz4
 conda create -n py35-np112 --yes -c conda-forge python=3.5 pip
 conda activate py35-np112
 pip install numpy==1.12 joblib==0.11.0 lz4
-conda create -n py36-np114 --yes -c conda-forge python=3.6 pip
-conda activate py36-np114
-pip install numpy==1.14 joblib==0.12.2 lz4
+conda create -n py36-np113 --yes -c conda-forge python=3.6 pip
+conda activate py36-np113
+pip install numpy==1.13 joblib==0.12.2 lz4
