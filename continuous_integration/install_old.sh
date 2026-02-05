e90b654d22663208681223dd94c21d39a168dcce
Nanored4498 <nanored.mc@gmail.com>
2026-02-05 16:07:29 +0100

    install joblbi with pip

--- continuous_integration/install_old.sh
+++ continuous_integration/install_old.sh
@@ -9,8 +9,18 @@ set -xe
 
 conda config --set solver libmamba
 
-conda create -n py27-np16 --yes -c conda-forge python=2.7 numpy=1.6 lz4 joblib=0.8.4
-conda create -n py27-np18 --yes -c conda-forge python=2.7 numpy=1.8 lz4 joblib=0.9.2
-conda create -n py34-np110 --yes -c conda-forge python=3.4 numpy=1.10 lz4 joblib=0.10.0
-conda create -n py35-np112 --yes -c conda-forge python=3.5 numpy=1.12 lz4 joblib=0.11.0
-conda create -n py36-np114 --yes -c conda-forge python=3.6 numpy=1.14 lz4 joblib=0.12.2
+conda create -n py27-np16 --yes -c conda-forge python=2.7 pip numpy=1.6 lz4
+conda activate py27-np16
+pip install joblib==0.8.4
+conda create -n py27-np18 --yes -c conda-forge python=2.7 pip numpy=1.8 lz4
+conda activate py27-np18
+pip install joblib==0.9.2
+conda create -n py34-np110 --yes -c conda-forge python=3.4 pip numpy=1.10 lz4
+conda activate py34-np110
+pip install joblib==0.10.0
+conda create -n py35-np112 --yes -c conda-forge python=3.5 pip numpy=1.12 lz4
+conda activate py35-np112
+pip install joblib==0.11.0
+conda create -n py36-np114 --yes -c conda-forge python=3.6 pip numpy=1.14 lz4
+conda activate py36-np114
+pip install joblib==0.12.2
