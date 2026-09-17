2024885f303136372832e376222d6eaf2ffe09e1
Nanored4498 <nanored.mc@gmail.com>
2026-09-17 14:42:46 +0200

    don't use declare

--- continuous_integration/install_old.sh
+++ continuous_integration/install_old.sh
@@ -6,11 +6,11 @@ conda config --set solver libmamba
 
 for PRE in "BEFORE_" "" "AFTER_"; do
     CONDA_ENV="${PRE}oldest"
-    delcare -n PYTHON_VERSION="${PRE}OLDEST_PYTHON_VERSION"
-    delcare -n NUMPY_VERSION="${PRE}OLDEST_NUMPY_VERSION"
-    delcare -n JOBLIB_VERSION="${PRE}OLDEST_JOBLIB_VERSION"
+    PYTHON_VERSION="${PRE}OLDEST_PYTHON_VERSION"
+    NUMPY_VERSION="${PRE}OLDEST_NUMPY_VERSION"
+    JOBLIB_VERSION="${PRE}OLDEST_JOBLIB_VERSION"
 
-    conda create -n $CONDA_ENV --yes -c conda-forge python=$PYTHON_VERSION pip
-    conda activate $CONDA_ENV
-    pip install numpy==$NUMPY_VERSION joblib==$JOBLIB_VERSION lz4
+    conda create -n $CONDA_ENV --yes -c conda-forge python=${!PYTHON_VERSION} pip
+    conda activate CONDA_ENV
+    pip install numpy==${!NUMPY_VERSION} joblib==${!JOBLIB_VERSION} lz4
 done
