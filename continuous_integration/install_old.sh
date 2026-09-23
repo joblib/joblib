8ad9453ece348f1fbae166e1f7689bbf14743a78
Nanored4498 <nanored.mc@gmail.com>
2026-09-23 18:05:44 +0200

    get package version from pypi

--- continuous_integration/install_old.sh
+++ continuous_integration/install_old.sh
@@ -2,15 +2,20 @@
 
 set -xe
 
+
+get_version() {
+    python -c "from versions import get_oldest_pypy_package_version; print(get_oldest_pypy_package_version('$1', '$2'))"
+}
+
 conda config --set solver libmamba
 
 for PRE in "BEFORE_" "" "AFTER_"; do
     CONDA_ENV="${PRE}oldest"
     PYTHON_VERSION="${PRE}OLDEST_PYTHON_VERSION"
-    NUMPY_VERSION="${PRE}OLDEST_NUMPY_VERSION"
-    JOBLIB_VERSION="${PRE}OLDEST_JOBLIB_VERSION"
+    NUMPY_VERSION=$(get_version numpy ${!PYTHON_VERSION})
+    JOBLIB_VERSION=$(get_version joblib ${!PYTHON_VERSION})
 
     conda create -n $CONDA_ENV --yes -c conda-forge python=${!PYTHON_VERSION} pip
     conda activate $CONDA_ENV
-    pip install numpy==${!NUMPY_VERSION} joblib==${!JOBLIB_VERSION} lz4
+    pip install numpy==$NUMPY_VERSION joblib==$JOBLIB_VERSION lz4
 done
