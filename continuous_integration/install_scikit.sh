84be34fcbcddf2499814c6e4d48ddc945a0420c6
Nanored4498 <nanored.mc@gmail.com>
2026-09-24 11:56:36 +0200

    simplify install

--- continuous_integration/install_scikit.sh
+++ continuous_integration/install_scikit.sh
@@ -4,6 +4,5 @@ conda config --set solver libmamba
 conda create -n testenv --yes -c conda-forge python=$LATEST_PYTHON_VERSION pip pytest scipy
 conda activate testenv
 
-pip install numpy cython # really necessary??
 pip install -v .
 pip install --pre --extra-index https://pypi.anaconda.org/scientific-python-nightly-wheels/simple scikit-learn
